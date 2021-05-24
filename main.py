8a47e558d2292e569bd4a51f69a28d9b53f37552
Sumitra <sumitrac2008@gmail.com>
2021-05-24 15:43:10 -0700

    Setup and Wrote pseudocode

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+#installed request in local machine 
 import requests
+from video_store import Videostore
+
 
 URL = "http://127.0.0.1:5000"
 BACKUP_URL = "https://retro-video-store-api.herokuapp.com"
@@ -10,3 +13,28 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+options = {
+    "1": "Create a video",
+    "2": "Update selected video",
+    "3": "Delete selected video",
+    "4": "list all videos",
+    "5": "Select a video", 
+
+    "6": "Create a customer",
+    "7": "Update selected customer",
+    "8": "Delete selected customer",
+    "9": "Select a customer", 
+    "10": "Select all customers",
+
+    "11": "Mark selected video complete",
+    "12": "Mark selected video incomplete",
+
+    "13": "Quit"
+    }
+
+
+
+
+#feel free to use Class 
+# ada Resources - Slack CLI 
---
+++ video_store.py
@@ -0,0 +1,43 @@
+import requests 
+import datetime 
+
+class Videostore:
+    def __init__(self, url="http://localhost:5000", selected_video=None):
+        self.url = url
+        self.selected_video = selected_video
+    
+    def add_video():
+        pass
+
+
+#all for videos 
+    #1 - add a video so that I can check it out to the customers
+
+    #2 - edit a video so that information about the video is accurate 
+
+    #3 - delete a video so that the store records stay up to date
+
+    #4 - get information about all videos so that I can see the store stock
+
+    #5 - get information about one video so that I can see how many copies are available 
+
+
+#all for customer 
+    #6 - add a customer so that I can check out videos to the customer
+
+    #7 - edit a customer so that the information about the customer is accurate 
+
+    #8 - delete a customer so that the customer is no longer in the store records 
+
+    #9 - get informaton about one customer so that I can see how many videos a customer had rented currently 
+
+    #10 - get information about all customers so that I can how many customers the store has
+
+# relationship between customer and video 
+    #11 - check out a video to a customer so that the store has a record of who has the video 
+
+    #12 - check in a video from a customer so that the video will be available to other customers
+
+# get out of the program 
+    #13 - Quit 
+
