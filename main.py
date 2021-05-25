88dbf4e8be4de28bfa47b6812dccfc98db3df35c
alison <alimcginley@gmail.com>
2021-05-25 16:49:21 -0700

    adds options to main.py

--- main.py
+++ main.py
@@ -4,9 +4,27 @@ import requests
 URL = "http://127.0.0.1:5000"
 BACKUP_URL = "https://retro-video-store-api.herokuapp.com"
 
+def list_options():
+    options = {
+        "1" : "Add a Video", 
+        "2" : "Edit a Video",
+        "3" : "Delete a Video",
+        "4" : "Browse all Videos",
+        "5" : "Select one Video",
+        "6" : "Add a new customer",
+        "7" : "Update customer profile",
+        "8" : "Remove customer",
+        "9" : "View customer profile",
+        "10" : "View all customers",
+        "11" : "Check out a Video",
+        "12" : "Return a Video"
+    }
+    for choice in options:
+        print(f"Option {choice}: {options[choice]}")
 def main():
     print("WELCOME TO RETRO VIDEO STORE")
-    pass
+    print("Please select from the following options")
+    list_options()
 
 
 if __name__ == "__main__":
--- video-store.py
+++ video-store.py
@@ -1,11 +1,9 @@
 import requests
 
 class VideoStore:
-    def __init__(self, url = "https://retro-video-store-api.herokuapp.com", video, customer, rental):
+    def __init__(self, url = "https://retro-video-store-api.herokuapp.com", selected_video = None):
         self.url = url
-        self.video = video 
-        self.customer = customer
-        self.rental = rental
+        self.selected_video = selected_video
 
     def add_video(self, title = "Default Title", release_date = None, total_inventory = None):
         query_params = {
@@ -14,4 +12,20 @@ class VideoStore:
             "total_inventory" : total_inventory
         }
         response = requests.post(self.url+"/videos", json = query_params)
+        return response.json()
+    
+    def update_video(self, title, release_date, total_inventory):
+        if not title:
+            title = self.selected_video["title"]
+        if not release_date:
+            release_date = self.selected_video["release_date"]
+        if not total_inventory:
+            total_inventory = self.selected_video["total_inventory"]
+        query_params = {
+            "title" : title,
+            "release date" : release_date,
+            "total_inventory" : total_inventory
+        }
+        response = requests.put(self.url+f"/videos/{self.selected_video['id']}", json=query_params)
+        self.selected_video = response.json()["video"]
         return response.json()
