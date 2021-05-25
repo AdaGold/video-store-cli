239e2e3e3bfe5eb09c29359bcd90ebd7097600ef
Courtney <courtneycmassey@gmail.com>
2021-05-24 22:22:41 -0700

    options 1, 3, 4, 5, 13 working

--- main.py
+++ main.py
@@ -22,24 +22,49 @@ def run_cli(play=True):
             total_inventory=input("How many copies of this movie are being added to the store? ")
 
             response = video_store.create_video(title=title, release_date=release_date, total_inventory=total_inventory)
-
             print(f"New video ID: {response['id']}")
 
-    # options = {
-    #     "1": "Add a Video", 
-    #     "2": "Edit a Video",
-    #     "3": "Delete a Video",
-    #     "4": "Get all Videos",
-    #     "5": "Get One Video",
-    #     "6": "Add a Customer",
-    #     "7": "Edit a Customer",
-    #     "8": "Delete a Customer",
-    #     "9": "Get Customer Info for One Customer",
-    #     "10": "Get Customer Info for All Customers",
-    #     "11": "Check OUT a Video",
-    #     "12": "Check IN a Video"
-    #     }
+        # elif choice=='2':
+        #     print(f"Great! Let's update the task: {task_list.selected_task}")
+        #     title=input("What is the new title of your task? ")
+        #     description=input("What is the new description of your task? ")
+        #     response = task_list.update_task(title=title, description=description)
 
+        #     print_stars()
+        #     print("Updated task:", response["task"])
+        
+        elif choice=='3':
+            video = select_video(video_store)
+            if video == None:
+                print("No video with that ID or title found.")
+            else:
+                print(f"You selected this video to delete: {video}")
+                print(f"Are you sure you want to delete?")
+                certainty = input(f"Select Y or N: ")
+                if certainty == "Y":
+                    video_store.delete_video()
+                    print(f"Video #{video['id']} successfully deleted.")
+                # do something if they select N, don't want to delete
+        
+        
+        elif choice=='4':
+            for video in video_store.list_videos():
+                print(video)
+        
+        elif choice=='5':
+            video = select_video(video_store)
+            if video == None:
+                print("No video with that ID or title found.")
+            else:
+                print(f"Selected video: {video}")
+
+
+        elif choice=='13':
+            play=False
+            print("Bye!")
+        
+        # elif choice=='M':
+        #     list_options()
 
     # print("WELCOME TO RETRO VIDEO STORE")
     # print("Select the information you want to interact with: ")
@@ -98,6 +123,23 @@ def make_choice(options, video_store):
     
     return choice
 
+def select_video(video_store):
+    print("Ok, first let's choose a video.")
+    select_by = input("Would you like to select by title (1) or ID (2)? ")
+    if select_by=="1":
+        title = input("Enter the name of the movie: ")
+        video_store.get_video(title=title)
+    elif select_by=="2":
+        id = input("Enter the movie ID number: ")
+        if id.isnumeric():
+            id = int(id)
+            video_store.get_video(id=id)
+    
+    # need a way to catch dumb user who doesn't enter 1 or 2 
+    # else:
+    #     print("Please enter 1 for title or 2 for ID: ")
+    
+    return video_store.selected_video 
 
 def video_options():
     pass
--- video_store.py
+++ video_store.py
@@ -6,12 +6,56 @@ class VideoStore:
         self.selected_video = selected_video
         self.selected_customer = selected_customer
 
+    # "1": "Add a Video" 
     def create_video(self, title="default", release_date="default", total_inventory=0):
+        #consider validating release date format because if it's entered incorrectly, CRASH!
         query_params = {
             "title": title,
             "release_date": release_date,
             "total_inventory": total_inventory
         }
-
         response = requests.post(self.url+"/videos", json=query_params)
         return response.json()
+
+    # "2": "Edit a Video"
+
+
+    # "3": "Delete a Video"
+    def delete_video(self):
+        response = requests.delete(self.url+f"/videos/{self.selected_video['id']}")
+        self.selected_video = None
+        return response.json()
+
+    # "4": "Get all Videos"
+    def list_videos(self):
+        response = requests.get(self.url+"/videos")
+        return response.json()
+
+    # "5": "Get One Video"
+    def get_video(self, title=None, id=None):
+    
+        for video in self.list_videos():
+            if title:
+                if video["title"]==title:
+                    id = video["id"]
+                    self.selected_video = video
+            elif id == video["id"]:
+                self.selected_video = video
+
+        if self.selected_video == None:
+            return None
+
+        response = requests.get(self.url+f"/videos/{id}")
+        return response.json()
+
+    # "6": "Add a Customer"
+    # "7": "Edit a Customer"
+    # "8": "Delete a Customer"
+    # "9": "Get Customer Info for One Customer"
+    # "10": "Get Customer Info for All Customers"
+    # "11": "Check OUT a Video"
+    # "12": "Check IN a Video"
+    # "13": "QUIT"
+
+
+
