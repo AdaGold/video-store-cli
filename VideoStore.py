b11f0592431f18cb3b5d80120c791352013af6ab
Brittney Payne <ohoptional@gmail.com>
2021-05-25 10:20:58 -0700

    Added requirements and video manipulation methods

---
+++ VideoStore.py
@@ -0,0 +1,47 @@
+import requests
+import datetime as dt 
+import maya
+
+
+
+
+class VideoStore:
+    def __init__(self, url="http://localhost:5000", selected_video=None, selected_customer=None):
+        self.url = url
+        self.selected_video = selected_video
+        self.selected_customer = selected_customer
+
+
+    def get_all_videos(self):
+        response = requests.get(self.url+"/videos")
+        return response.json()
+    
+    def get_single_video(self, id=None, title=None):
+        for video in self.get_all_videos():
+            if video["id"] == id:
+                self.selected_video = video
+            elif video["title"] == title:
+                self.selected_video = video
+        if self.selected_video == None:
+            return "Could not find that video."
+        return self.selected_video
+
+    def edit_single_video(self, selected_video):
+        new_title = input(f'Update the title for video {selected_video["id"]}:')
+        new_release_date = input(f"Update the release date for video {selected_video['id']}:  ")
+        date_time_object= maya.parse(new_release_date).datetime()
+        new_total_inventory = input(f"Update the total inventory for video {selected_video['id']}:  ")
+        print(date_time_object)
+        query_params = {
+            "title": new_title, 
+            "release_date": str(date_time_object),
+            "total_inventory": new_total_inventory
+        }
+        response = requests.put(self.url+f'/videos/{selected_video["id"]}', json=query_params)
+        if response:
+            print("Success!",  response)
+        else: 
+            print("Oops! Something went wrong. ")
+        self.selected_video = response.json()
+        return response.json()
+
--- main.py
+++ main.py
@@ -1,11 +1,88 @@
 import requests
+from VideoStore import VideoStore
 
-URL = "http://127.0.0.1:5000"
-BACKUP_URL = "https://retro-video-store-api.herokuapp.com"
+URL = "http://localhost:5000"
+BACKUP_URL = "https://bp-retro-video-store.herokuapp.com/"
+
+
+def print_linebreaks():
+    print("\n#############################\n")
+
+def main_menu_options(): 
+    
+    print("1: Video Information")
+    print("2: Customer Information")
+    print("3: Rental Information")
+                
+
+
+def video_menu_options():
+    
+    print("1:  List All Videos")
+    print("2:  Select A Video To Edit")
+    print("3:  Select A Video To Delete")
+
+
+    
+def customer_menu_options():
+    customer_menu = {
+                "1": "List All Customers",
+                "2": "Select A Customer",
+                "3": "Edit Selected Customer", 
+                "4": "Delete Selected Customer"
+
+    }
+    return customer_menu
+
+def rental_menu_options():
+    rental_menu = {
+                "1": "Check Out A Video", 
+                "2": "Check In A Video"
+    }
+    
+def print_video_info(video):
+    print("\n")
+    print(f"Title: {video['title']}")
+    print(f"ID: {video['id']}")
+    print(f"Release Date: {video['release_date']}")
+    print(f"Total Inventory: {video['total_inventory']}")
+    print(f"Available Inventory: {video['available_inventory']}")
+    print("\n")
+
+
 
 def main():
+
     print("WELCOME TO RETRO VIDEO STORE")
-    pass
+    print_linebreaks()
+    print("PLEASE CHOOSE FROM THE FOLLOWING MENU: ")
+    main_menu_options()
+    user_input = input("::")
+    if user_input == "1":
+        video_store = VideoStore()
+        print("PLEASE CHOOSE FROM THE FOLLOWING MENU: ")
+        video_menu_options()
+        video_user_input = input("::")
+        if video_user_input == "1":
+            all_videos = video_store.get_all_videos()
+            for video in all_videos:
+                print_video_info(video)
+
+        elif video_user_input == "2":
+            user_input = input("Please enter the ID of the video you would like to select:  ")
+            selected_video = video_store.get_single_video(id=int(user_input))
+            print_video_info(selected_video)
+            video_store.edit_single_video(selected_video)
+
+        
+        elif video_user_input == "3":
+            pass
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
