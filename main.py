59b640270271e9c14fd4200a6258787f5072ea37
Sara <sarajessvega17@gmail.com>
2021-05-25 10:21:35 -0500

    Gitignore updated

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ BACKUP_URL = "https://retro-video-store-api.herokuapp.com"
 
 def main():
     print("WELCOME TO RETRO VIDEO STORE")
+    choice = input("Pick something! ")
+    
+    if choice == "1":
+        response = requests.get(BACKUP_URL +"/videos")
+        print(response.json())
     pass
-
 
 if __name__ == "__main__":
     main()
