8321b6e0d12ef83b81d5f3fe62712611f4bcfe00
Raquel Mena <raquelmena03@gmail.com>
2021-05-26 13:15:52 -0500

    roadmap written, main and customer py files create

---
+++ customer.py
@@ -0,0 +1,43 @@
+import requests
+import datetime
+
+class Customer:
+    def __init__(self, url="http://localhost:5000", selected_customer=None):
+        self.url = url
+        #self.selected_customer = selected_customer #or selected_action?
+
+    def create_customer(self, customer_name="Default Name", postal_code="0", phone_number="0"):
+        query_params = {
+            "name": customer_name,
+            "postal_code": postal_code,
+            "phone": phone_number
+        }
+        response = requests.post(self.url + "/customers", json=query_params)
+        return response.json()
+
+    def update_customer_info(self, customer_name="Default Name", postal_code="0", phone_number="0" ):
+        query_params = {
+            "name": customer_name,
+            "postal_code": postal_code,
+            "phone": phone_number
+        }
+        response = requests.post(self.url + "/customers/{customer_id}", json=query_params)
+        return response.json()
+
+    def all_customers(self):
+        response = requests.get(self.url+"/customers")
+        return response.json()
+
+    def get_customer(self, customer_id=None):
+        for customer in self.all_customers():
+            if customer_id:
+                if customer["customer_id"]==customer_id:
+                # id = customer["id"]
+                    self.selected_customer = customer
+            if self.selected_customer == None:
+                return "No customer found"
+            else:
+                return "ID necessary"
+        
+        response = requests.get(self.url+f"/customers/{customer_id}")
+        return response.json()
--- main.py
+++ main.py
@@ -1,12 +1,78 @@
 import requests
+from customer import Customer
 
 URL = "http://127.0.0.1:5000"
 BACKUP_URL = "https://retro-video-store-api.herokuapp.com"
 
+
+def list_options(): 
+    options = {
+        "1": "Add New Reel", 
+        "2": "Edit Reel Information",
+        "3": "Delete Reel", 
+        "4": "Get All Reel Information", 
+        "5": "Lookup Specific Reel", 
+        "6": "Add Customer",
+        "7": "Update Customer Information",
+        "8": "Delete Customer",
+        "9": "Get All Customers ",
+        "10": "Get Specific Customer",
+        "11": "Checkout Reel to Customer",
+        "12": "Check-in Reel",
+        "13": "Quit"
+        }
+
+    print("Welcome to the RAQIS REELS CLI")
+    print("These are the actions you can perform")
+
+    for choice_num in options:
+        print(f"Option {choice_num}. {options[choice_num]}")
+
+    return options
+
+def make_choice(options):
+    valid_choices = options.keys()
+    choice = None
+
+    while choice not in valid_choices:
+        print("Not a valid selection, please select again")
+        choice = input("Make your selection using the option number: ")
+
+#     if choice in ['6','7','8']:
+#         print("")
+#         print("")
+#         choice = "3"
+    
+#     return choice
+
+def run_cli(play=True):
+
+    customer = Customer(url="http://127.0.0.1:5000")
+
+    options = list_options()
+
+    while play==True:
+        choice = make_choice(options)
+
+    if choice=='8':
+        #print_stars()
+        for customer in customer.all_customers():
+                print(customer)
+    if choice =="10":
+        customer_id = input("Please enter valid customer ID:  ")
+        if customer_id.isnumeric():
+            customer_id = int(customer_id)
+            customer.get_customer(customer_id=customer_id)
+
+    
+
 def main():
     print("WELCOME TO RETRO VIDEO STORE")
-    pass
-
+    #look up snowman project
+    list_options()
+    customer = Customer() 
+    print(customer.all_customers())
+    
 
 if __name__ == "__main__":
-    main()
+    run_cli()
