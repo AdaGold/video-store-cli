d693601950fd4173d7a35629faaa22aee4ea7384
Summer <summmick@gmail.com>
2021-05-26 16:18:07 -0700

    Create video requests

--- main.py
+++ main.py
@@ -5,8 +5,24 @@ BACKUP_URL = "https://retro-video-store-api.herokuapp.com"
 
 def main():
     print("WELCOME TO RETRO VIDEO STORE")
-    pass
-
+    
 
 if __name__ == "__main__":
     main()
+
+def list_options():
+
+    options = {
+        "1": "Add a Video",
+        "2": "Edit a Video",
+        "3": "Delete a Video",
+        "4": "Get all of the Videos Information",
+        "5": "Get one of the Videos Information",
+        "6": "Add a Customer",
+        "7": "Edit a Customer",
+        "8": "Delete a Customer",
+        "9": "Get all of the Customers Information",
+        "10": "Get one of the Customers Information",
+        "11": "Check out a Video to a Customer",
+        "12": "Check in a Video from a Customer"
+    }
