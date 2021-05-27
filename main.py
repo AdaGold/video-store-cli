4112a4be84304a5fbefea1a2a4217d2ef11a0ae7
Nataliia Getlin <natali.getlin@gmail.com>
2021-05-26 22:26:02 -0700

    - create Edit a video - create Add a customer - create Edit a customer - create Delete a customer - create Get info about all customers - create Get info on spec customer - create Check-out - create Check-in

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import requests
 from requests.api import options
 from video_store import Employee
 
@@ -11,12 +12,13 @@ def list_actions_on():
     actions_on = {
         "1": "Videos",
         "2": "Customers",
-        "3": "Quit"
+        "3": "Rentals",
+        "6": "Quit"
     }
 
     print_stars()
     print("Welcome to the Retro Video Store CLI")
-    print("Make your choice. You can performe actions on:")
+    print("Make your choice. You can manage:")
     print_stars()
 
     for choice_num in actions_on:
@@ -57,9 +59,7 @@ def list_actions_customer():
         "3": "Delete a customer",
         "4": "Get information about all customers",
         "5": "Get information about one customer",
-        "6": "Check-out a video to a customer",
-        "7": "Check-in a video to a customer",
-        "8": "Quit"
+        "6": "Quit"
     }
 
     print_stars()
@@ -75,9 +75,30 @@ def list_actions_customer():
     return actions_customer
 
 
-def make_choice():
+def list_actions_rentals():
+
+    actions_rentals = {
+        "1": "Check-out a video to a customer",
+        "2": "Check-in a video to a customer",
+        "6": "Quit"
+    }
+
     print_stars()
-    choice = input("Make your selection using the option number >> ")
+    print("What would you like to do? Make your choice.")
+    print("These are the actions you can perform on Rentals:")
+    print_stars()
+
+    for choice_num in actions_rentals:
+        print(f"Action {choice_num}. {actions_rentals[choice_num]}")
+
+    print_stars()
+
+    return actions_rentals
+
+
+def make_choice():
+    # print_stars()
+    choice = input("\nMake your selection using the option number >> ")
     return choice
 
 
@@ -87,15 +108,44 @@ def select_video(employee):
         video_id = int(video_id)
         employee.get_one_video(id=video_id)
     else:
-        print("Could not select a video. Please, enter ID.")
-    # return selected_video
+        print("Could not select a video. Please, enter valid ID.")
+
+
+def select_customer(employee):
+    customer_id = input("Enter customer ID >> ")
+    if customer_id.isnumeric():
+        customer_id = int(customer_id)
+        employee.get_one_customer(id=customer_id)
+    else:
+        print("Could not select a customer. Please, enter valid ID.")
+
+
+def print_video(video):
+    print(f"""
+        ID: {video["id"]}
+        Title: {video["title"]}
+        Release date: {video["release_date"]}
+        Total inventory: {video["total_inventory"]}
+        Available inventory: {video["available_inventory"]}
+        """)
+
+
+def print_customer(customer):
+    print(f"""
+        ID: {customer["id"]}
+        Name: {customer["name"]}
+        Phone: {customer["phone"]}
+        Postal code: {customer["postal_code"]}
+        Date Registered: {customer["registered_at"]},
+        Videos checked-out: {customer["videos_checked_out_count"]}
+        """)
 
 
 def run_video_store_cli(active=True):
 
     employee = Employee()
 
-    group_options = list_actions_on()
+    list_actions_on()
 
     while active:
         choice = make_choice()
@@ -112,10 +162,27 @@ def run_video_store_cli(active=True):
                                               release_date=release_date,
                                               total_inventory=total_inventory)
 
-                print("New video: ", response["title"])
+                print(
+                    "New video has been added: ",
+                    response["title"],
+                    "has ID",
+                    response["id"])
 
             elif choice == '2':
                 print("Edit a video")
+                print("Select video to edit.")
+                select_video(employee)
+                video = employee.selected_video
+                print_video(video)
+                new_title = input("Enter new title >> ")
+                new_release_date = input("Enter new release date >> ")
+                new_total_inventory = input("Enter new total inventory >> ")
+                response = employee.update_video(
+                    title=new_title,
+                    release_date=new_release_date,
+                    total_inventory=new_total_inventory)
+                print("Video has been updated. Here is the updated video.")
+                print_video(response)
 
             elif choice == '3':
                 print("Delete a video")
@@ -127,25 +194,14 @@ def run_video_store_cli(active=True):
             elif choice == '4':
                 print("Get information about all videos")
                 for video in employee.list_all_videos():
-                    print(f"""
-                         ID: {video["id"]}
-                         Title: {video["title"]}
-                         Release date: {video["release_date"]}
-                         Total inventory: {video["total_inventory"]}
-                         Available inventory: {video["available_inventory"]}
-                         """)
+                    print_video(video)
 
             elif choice == '5':
                 print("Get information about one video")
                 print("Select video.")
                 select_video(employee)
-                print(f"""
-                         ID: {employee.selected_video["id"]}
-                         Title: {employee.selected_video["title"]}
-                         Release date: {employee.selected_video["release_date"]}
-                         Total inventory: {employee.selected_video["total_inventory"]}
-                         Available inventory: {employee.selected_video["available_inventory"]}
-                         """)
+                video = employee.selected_video
+                print_video(video)
 
             elif choice == '6':
                 # Quit
@@ -153,28 +209,92 @@ def run_video_store_cli(active=True):
                 print("\nYou just stopped using the Retro Video Store CLI!")
 
         elif choice == "2":
-            options = list_actions_customer()
+            list_actions_customer()
             choice = make_choice()
 
             if choice == '1':
                 print("Add a customer")
+                name = input("Enter the customer name >> ")
+                postal_code = input("Enter the customer postal code >> ")
+                phone = input("Enter the customer phone >> ")
+                response = employee.add_customer(name=name,
+                                                 postal_code=postal_code,
+                                                 phone=phone)
+
+                print("New customer: ", response["name"])
+
             elif choice == '2':
                 print("Edit a customer")
+                print("Select customer to edit.")
+                select_customer(employee)
+                customer = employee.selected_customer
+                print_customer(customer)
+
+                name = input("Enter new customer name >> ")
+                postal_code = input("Enter new customer postal code >> ")
+                phone = input("Enter new customer phone >> ")
+                updated_customer = employee.update_customer(
+                    name=name,
+                    postal_code=postal_code,
+                    phone=phone)
+                print("Customer has been updated. Here is the updated customer.")
+                print_customer(updated_customer)
+
             elif choice == '3':
                 print("Delete a customer")
+                print("Select customer.")
+                select_customer(employee)
+                deleted_customer = employee.delete_customer()
+                print(
+                    f"Video with ID {deleted_customer['id']} has been deleted.")
             elif choice == '4':
                 print("Get information about all customers")
+                for customer in employee.list_all_customers():
+                    print_customer(customer)
             elif choice == '5':
                 print("Get information about one customer")
-            elif choice == '6':
+                print("Select customer.")
+                select_customer(employee)
+                customer = employee.selected_customer
+                print_customer(customer)
+            else:
+                # Quit
+                active = False
+                print("\nYou just stopped using the Retro Video Store CLI!")
+
+        elif choice == "3":
+            list_actions_rentals()
+            choice = make_choice()
+            if choice == '1':
                 print("Check-out a video to a customer")
-            elif choice == '7':
+                print("Select customer.")
+                select_customer(employee)
+                customer = employee.selected_customer
+                print("Select video.")
+                select_video(employee)
+                video = employee.selected_video
+                employee.check_out()
+                print(
+                    f"Video with ID {video['id']}, title '{video['title'].capitalize()}' checked-out to a customer {customer['name'].capitalize()} with ID {customer['id']}")
+
+            elif choice == '2':
                 print("Check-in a video to a customer")
-            elif choice == '8':
+                print("Select customer.")
+                select_customer(employee)
+                customer = employee.selected_customer
+                print("Select video.")
+                select_video(employee)
+                video = employee.selected_video
+                employee.check_in()
+                print(
+                    f"Video with ID {video['id']}, title '{video['title'].capitalize()}' checked-in from a customer {customer['name'].capitalize()} with ID {customer['id']}")
+
+            else:
                 # Quit
                 active = False
                 print("\nYou just stopped using the Retro Video Store CLI!")
         else:
+            # Quit
             active = False
             print("\nYou just stopped using the Retro Video Store CLI!")
 
--- video_store.py
+++ video_store.py
@@ -1,10 +1,17 @@
+# from main import select_customer
 import requests
 
 
 class Employee:
-    def __init__(self, url="http://localhost:5000", selected_video=None):
+    def __init__(
+            self,
+            url="http://localhost:5000",
+            selected_video=None,
+            selected_customer=None):
+
         self.url = url
         self.selected_video = selected_video
+        self.selected_customer = selected_customer
 
     def add_video(
             self,
@@ -29,8 +36,39 @@ class Employee:
             if video["id"] == id:
                 self.selected_video = video
 
-    def update_video(self):
-        pass
+        # if self.selected_video is None:
+        #     return "Could not find this video."
+
+        # response = requests.get(self.url + f"/videos/{id}")
+        # return response.json()
+
+    def update_video(
+            self,
+            title=None,
+            release_date=None,
+            total_inventory=None):
+
+        if not title:
+            title = self.selected_video["title"]
+        if not release_date:
+            release_date = self.selected_video["release_date"]
+        if not total_inventory:
+            total_inventory = self.selected_video["total_inventory"]
+
+        query_params = {
+            "title": title,
+            "release_date": release_date,
+            "total_inventory": int(total_inventory)
+        }
+        # print(query_params)
+
+        response = requests.put(
+            self.url +
+            f"/videos/{self.selected_video['id']}",
+            json=query_params)
+        # print("response:", response)
+
+        return response.json()
 
     def delete_video(self):
         response = requests.delete(
@@ -38,6 +76,94 @@ class Employee:
         self.selected_video = None
         return response.json()
 
+    def add_customer(
+            self,
+            name="Default Name",
+            postal_code=10000,
+            phone="(000) 000-0000"):
 
-class Customer:
-    pass
+        query_params = {
+            "name": name,
+            "postal_code": postal_code,
+            "phone": phone
+        }
+
+        response = requests.post(self.url + "/customers", json=query_params)
+        return response.json()
+
+    def list_all_customers(self):
+        response = requests.get(self.url + "/customers")
+        return response.json()
+
+    def get_one_customer(self, id=None):
+        for customer in self.list_all_customers():
+            if customer["id"] == id:
+                self.selected_customer = customer
+
+    def delete_customer(self):
+        response = requests.delete(
+            self.url + f"/customers/{self.selected_customer['id']}")
+        self.selected_video = None
+        return response.json()
+
+    def update_customer(
+            self,
+            name=None,
+            postal_code=None,
+            phone=None):
+
+        if not name:
+            name = self.selected_customer["name"]
+        if not postal_code:
+            postal_code = self.selected_customer["postal_code"]
+        if not phone:
+            phone = self.selected_customer["phone"]
+
+        query_params = {
+            "name": name,
+            "postal_code": int(postal_code),
+            "phone": phone
+        }
+        # print(query_params)
+
+        response = requests.put(
+            self.url +
+            f"/customers/{self.selected_customer['id']}",
+            json=query_params)
+        # print("response:", response)
+
+        return response.json()
+
+    def check_out(self, customer_id=None, video_id=None):
+
+        if not customer_id:
+            customer_id = self.selected_customer["id"]
+        if not video_id:
+            video_id = self.selected_video["id"]
+
+        query_params = {
+            "customer_id": customer_id,
+            "video_id": video_id
+        }
+
+        response = requests.post(
+            self.url + "/rentals/check-out",
+            json=query_params)
+        return response.json()
+
+    def check_in(self, customer_id=None, video_id=None):
+
+        if not customer_id:
+            customer_id = self.selected_customer["id"]
+        if not video_id:
+            video_id = self.selected_video["id"]
+
+        query_params = {
+            "customer_id": customer_id,
+            "video_id": video_id
+        }
+
+        response = requests.post(
+            self.url + "/rentals/check-in",
+            json=query_params)
+        return response.json()
