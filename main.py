126db742f18742907b4d9845636cdb73e0eebc75
Priscille Gwet <ngomakak@gmail.com>
2021-05-28 10:36:41 -0700

    Added space inside input functions
    Added art.py file
    
    Added a log

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from os import name
 import requests
 from requests.api import options
 from video_store import Employee
+from art import logo
 
 URL = "http://127.0.0.1:5000"
 BACKUP_URL = "https://retro-video-store-api.herokuapp.com"
+
+def print_stars():
+    print("\n**************************\n")
 
 def list_of_options():
     options = {
@@ -14,7 +18,7 @@ def list_of_options():
         "4": "To quit"
     }
 
-    print("Please choose from the following menu options")
+    print("Please choose from the following menu options:")
     for choice_num in options:
         print(f"{options[choice_num]}, type {choice_num}")
     return options
@@ -23,7 +27,7 @@ def make_a_choice(options):
     valid_choices = options.keys()
     choice = None
     while choice not in valid_choices:
-        choice = input("::")
+        choice = input(":: ")
     return choice
 
 def manage_customers():
@@ -36,7 +40,7 @@ def manage_customers():
         "6": "To return to the main menu",
     }
 
-    print("Please choose from the following Customer menu options")
+    print("Please choose from the following Customer menu options:")
     for choice_num in options:
         print(f"{options[choice_num]}, type {choice_num}")
     return options
@@ -45,7 +49,7 @@ def make_a_choice_in_customer_menu_options(options, store_employee):
     valid_choices = options.keys()
     choice = None
     while choice not in valid_choices:
-        choice = input("::")
+        choice = input(":: ")
     if choice in ['4','5'] and store_employee.selected_customer == None:
         print("You must select a customer before updating it or deleting it.")
         print("Let's select a customer!")
@@ -62,7 +66,7 @@ def manage_videos():
         "6": "To return to the main menu",
     }
 
-    print("Please choose from the following Video menu options")
+    print("Please choose from the following Video menu options:")
     for choice_num in options:
         print(f"{options[choice_num]}, type {choice_num}")
     return options
@@ -71,7 +75,7 @@ def make_a_choice_in_video_menu_options(options, store_employee):
     valid_choices = options.keys()
     choice = None
     while choice not in valid_choices:
-        choice = input("::")
+        choice = input(":: ")
     if choice in ['4','5'] and store_employee.selected_video == None:
         print("You must select a video before updating it or deleting it.")
         print("Let's select a video!")
@@ -85,7 +89,7 @@ def manage_rentals():
         "3": "To return to the main menu",
     }
 
-    print("Please choose from the following Rental menu options")
+    print("Please choose from the following Rental menu options:")
     for choice_num in options:
         print(f"{options[choice_num]}, type {choice_num}")
     return options
@@ -96,14 +100,19 @@ def main():
     
     quit = False
     while not quit:
-    # print art 
+        print(logo)
+        print_stars()
         print("WELCOME TO RETRO VIDEO STORE")
+        print_stars()
         options = list_of_options()
+        print_stars()
         choice = make_a_choice(options)
         if choice == "1":
             exit = False
             while not exit:
+                print_stars()
                 customer_options = manage_customers()
+                print_stars()
                 option_chosen= make_a_choice_in_customer_menu_options(customer_options, store_employee)
                 if option_chosen == "1":
                     for customer in store_employee.get_all_customers():
@@ -111,10 +120,10 @@ def main():
                 elif option_chosen == "2":
                     select_by = input("Would you like to select by name or id?  ")
                     if select_by == "name":
-                        name = input("Which customer would you like to select?")
+                        name = input("Which customer name would you like to select? ")
                         store_employee.get_a_customer(name=name)
                     elif select_by == "id":
-                        id = input("Which customer id would you want to select?")
+                        id = input("Which customer id would you want to select? ")
                         if id.isnumeric():
                             id = int(id)
                             store_employee.get_a_customer(id=id)
@@ -125,17 +134,17 @@ def main():
                     if not store_employee.selected_customer:
                         print("Could not find a customer by that name or id")
                 elif option_chosen == "3":
-                    name =input("What is the name of your customer?")
-                    postal_code =input("What is the customer's postal code?")
-                    phone =input("What is the customer's phone number?")
+                    name =input("What is the name of your customer? ")
+                    postal_code =input("What is the customer's postal code? ")
+                    phone =input("What is the customer's phone number? ")
                     response = store_employee.add_customer(name=name, postal_code=postal_code, phone=phone)
                     print("New customer", response)
                 elif option_chosen == "4":
                     print(f"Great! Let's update the customer: {store_employee.selected_customer}")
-                    name =input("What is the new name of your customer?")
+                    name =input("What is the new name of your customer? ")
                     print(name)
-                    postal_code =input("What is the new customer's postal code?")
-                    phone =input("What is the new customer's phone number?")
+                    postal_code =input("What is the new customer's postal code? ")
+                    phone =input("What is the new customer's phone number? ")
                     response = store_employee.update_customer(name=name, postal_code=postal_code, phone=phone)
                     print(response)
                 elif option_chosen == "5":
@@ -146,7 +155,9 @@ def main():
         elif choice == "2":
             exit = False
             while not exit:
+                print_stars()
                 video_options = manage_videos()
+                print_stars()
                 option_chosen= make_a_choice_in_video_menu_options(video_options, store_employee)
                 if option_chosen == "1":
                     for video in store_employee.get_all_videos():
@@ -154,10 +165,10 @@ def main():
                 elif option_chosen == "2":
                     select_by = input("Would you like to select by? Enter title or id: ")
                     if select_by == "title":
-                        title = input("Which video would you like to select?")
+                        title = input("Which video title would you like to select? ")
                         store_employee.get_a_video(title=title)
                     elif select_by == "id":
-                        id = input("Which video id would you want to select?")
+                        id = input("Which video id would you want to select? ")
                         if id.isnumeric():
                             id = int(id)
                             store_employee.get_a_video(id=id)
@@ -169,16 +180,16 @@ def main():
                     if not store_employee.selected_video:
                         print("Could not find a video by that title or id")    
                 elif option_chosen == "3":
-                    title =input("What is the title of your video?")
-                    release_date =input("What is the video's release date?")
-                    total_inventory =input("What is the video's total_inventory?")
+                    title =input("What is the title of your video? ")
+                    release_date =input("What is the video's release date? ")
+                    total_inventory =input("What is the video's total_inventory? ")
                     response = store_employee.add_video(title=title, release_date=release_date, total_inventory=total_inventory)
                     print("New video", response)
                 elif option_chosen == "4":
                     print(f"Great! Let's update the video: {store_employee.selected_video}")
-                    title =input("What is the new title of your video?")
-                    release_date =input("What is the new video's release date?")
-                    total_inventory =input("What is the new video's total_inventory?")
+                    title =input("What is the new title of your video? ")
+                    release_date =input("What is the new video's release date? ")
+                    total_inventory =input("What is the new video's total_inventory? ")
                     response = store_employee.update_video(title=title, release_date=release_date, total_inventory=total_inventory)
                     print(response)
                 elif option_chosen == "5":
@@ -189,22 +200,26 @@ def main():
         elif choice == "3":
             exit = False
             while not exit:
+                print_stars()
                 rental_options = manage_rentals()
+                print_stars()
                 option_chosen= make_a_choice(rental_options)
                 if option_chosen == "1":
-                    customer_id = input("What is the customer id?")
-                    video_id = input("What is the video id?")
+                    customer_id = input("What is the customer id? ")
+                    video_id = input("What is the video id? ")
                     response = store_employee.check_out(customer_id=customer_id, video_id=video_id)
                     print(response)
                 elif option_chosen == "2":
-                    customer_id = input("What is the customer id?")
-                    video_id = input("What is the video id?")
+                    customer_id = input("What is the customer id? ")
+                    video_id = input("What is the video id? ")
                     response = store_employee.check_in(customer_id=customer_id, video_id=video_id)
                     print(response)
                 elif option_chosen == "3":
                     exit = True
         elif choice == "4":
-            print("Thanks for coming to our store!")   
+            print_stars()
+            print("Thanks for using our store!")
+            print_stars()  
             quit = True
 
 if __name__ == "__main__":
