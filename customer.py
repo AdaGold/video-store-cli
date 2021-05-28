5bc0c287fe5a25afe51cbb58888072b55b35bfd7
Lin Zhang <linzhang294@gmail.com>
2021-05-28 00:57:24 -0400

    adds customer class to front end

--- customer.py
+++ customer.py
@@ -2,38 +2,62 @@ import requests
 import datetime
 
 
-class Video:
-    def __init__(self, url="https://retro-video-store-api.herokuapp.com", selected_video=None):
+class Customer:
+    def __init__(self, url="https://retro-video-store-api.herokuapp.com", selected_customer=None):
         self.url = url
-        self.selected_video = selected_video
+        self.selected_customer = selected_customer
 
-    def create_video(self, name="Default Video", postal_code="Default Postal", phone="Default Phone"):
+    def create_customer(self, name="Default Customer", postal_code="Default Postal", phone="Default Phone"):
         query_params = {
             "name": name,
             "postal_code": postal_code,
             "phone": phone
         }
-        response = requests.post(self.url+"/videos", json=query_params)
+        response = requests.post(self.url+"/customers", json=query_params)
         return response.json()
 
-    def list_videos(self):
-        response = requests.get(self.url+"/videos")
+    def list_customers(self):
+        response = requests.get(self.url+"/customers")
         return response.json()
 
-    def get_video(self, name=None, id=None):
-        for video in self.list_videos():
+    def get_customer(self, name=None, id=None):
+        for customer in self.list_customers():
             if name:
-                if name["name"] == name:
-                    id = video["id"]
-                    self.selected_video = video
-            elif id == video["id"]:
-                self.selected_video = video
+                if name["title"] == name:
+                    id = customer["id"]
+                    self.selected_customer = customer
+            elif id == customer["id"]:
+                self.selected_customer = customer
 
-        if self.selected_video == None:
-            return "Could not find vieo by that name or id"
+        if self.selected_customer == None:
+            return "Could not find customer by that name or ID"
 
-        response = requests.get(self.url+f"/videos/{id}")
+        response = requests.get(self.url+f"/customers/{id}")
         return response.json()
 
-    def upgrade_video(self, name=None, description=None):
+    def upgrade_customer(self, id=None, name=None, postal_code=None, phone=None):
         if not name:
+            title = self.selected_customer["name"]
+        if not postal_code:
+            release_date = self.selected_customer["postal_code"]
+        if not phone:
+            total_inventory = self.selected_customer["phone"]
+
+        query_params = {
+            "name": name,
+            "postal_code": postal_code,
+            "phone": phone
+        }
+        response = requests.put(
+            self.url+f"/customers/{self.selected_customer['id']}",
+            json=query_params
+        )
+        print("response:", response)
+        # self.selected_video = response.json()["video"]
+        return response.json()
+
+    def delete_customer(self):
+        response = requests.delete(
+            self.url+f"/customers/{self.selected_customer['id']}")
+        self.selected_customer = None
+        return response.json()
--- video.py
+++ video.py
@@ -7,12 +7,12 @@ class Video:
         self.url = url
         self.selected_video = selected_video
 
-    def create_video(self, title="Default Video", release_date="Default Date", total_inventory="Default Total", available_inventory="Default Available"):
+    def create_video(self, title="Default Video", release_date="Default Date", total_inventory="Default Total"):
         query_params = {
             "title": title,
             "release_date": release_date,
             "total_inventory": total_inventory,
-            "available_inventory": available_inventory
+            "available_inventory": total_inventory
         }
         response = requests.post(self.url+"/videos", json=query_params)
         return response.json()
@@ -31,12 +31,12 @@ class Video:
                 self.selected_video = video
 
         if self.selected_video == None:
-            return "Could not find video by that title or id"
+            return "Could not find video by that title or ID"
 
         response = requests.get(self.url+f"/videos/{id}")
         return response.json()
 
-    def upgrade_video(self, title=None, release_date=None, total_inventory=None):
+    def upgrade_video(self, id=None, title=None, release_date=None, total_inventory=None):
         if not title:
             title = self.selected_video["title"]
         if not release_date:
@@ -54,7 +54,7 @@ class Video:
             json=query_params
         )
         print("response:", response)
-        self.selected_video = response.json()["video"]
+        # self.selected_video = response.json()["video"]
         return response.json()
 
     def delete_video(self):
