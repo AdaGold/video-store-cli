921a0c775b61566382f2e01c40e17422f874e170
Nyckolle Lucuab <66242273+NLucuab@users.noreply.github.com>
2021-05-27 21:53:21 -1000

    creates class modules and adds main.py logic

---
+++ customer.py
@@ -0,0 +1,47 @@
+import requests
+from datetime import datetime
+
+class Customer:
+    def __init__(self, url="https://retro-video-store-api.herokuapp.com", selected_customer=None):
+        self.url = url
+        self.selected_customer = selected_customer
+
+    def add_customer(self,name="Default Name",postal_code=None,phone=None, registered_at=None):
+        query_params = {
+            "name": name,
+            "postal_code": postal_code,
+            "phone": phone,
+            "registered_at": datetime.now()
+        }
+        response = requests.post(self.url+"/customers", json = query_params)
+        return response.json()
+    
+    def list_customers(self):
+        response = requests.get(self.url+"/customers")
+        return response.json()
+    
+    def get_customer(self, name=None, id=None):
+
+        for customer in self.list_customers():
+            if name:
+                if customer["name"]==name:
+                    id = customer["id"]
+                    self.selected_customer = customer
+            elif id == customer["id"]:
+                self.selected_customer = customer
+        
+        if self.selected_customer == None:
+            return "Could not find customer by that name or id"
+        
+        response = requests.get(self.url+f"/customers/{id}")
+        return response.json()
+
+    def update_customer(self, name=None, postal_code=None, phone=None):
+        if not name:
+            name = self.selected_customer["name"]
+        if not postal_code:
+            postal_code =  self.selected_customer["postal_code"]
+        if not phone:
+            phone = self.selected_customer["phone"]
+        
+
--- main.py
+++ main.py
@@ -1,12 +1,59 @@
 import requests
+from customer import Customer
+from rental import Rental
+from video import Video
 
-URL = "http://127.0.0.1:5000"
-BACKUP_URL = "https://retro-video-store-api.herokuapp.com"
+URL = "https://retro-video-store-api.herokuapp.com"
+def print_stars():
+    print("\n**************************\n")
 
-def main():
-    print("WELCOME TO RETRO VIDEO STORE")
-    pass
+def list_options():
+    options = {
+        "1": "Add a video",
+        "2": "Edit a video",
+        "3": "Delete a video",
+        "4": "Get information on all videos",
+        "5": "Get information on one video",
+        "6": "Add a customer",
+        "7": "Edit a customer",
+        "8": "Delete a customer",
+        "9": "Get information on one customer",
+        "10": "Get information on all customers",
+        "11": "Check out a video to a customer",
+        "12": "Check in a video from a customer",
+        "13": "List all options",
+        "14": "Quit"
+    }
+    print_stars()
+    print("Welcome to the Video Store CLI")
+    print("These are the actions you can perform")
+    print_stars()
 
+    for choice_num in options:
+        print(f"Option {choice_num}. {options[choice_num]}")
+    
+    print_stars()
+
+    return options
+
+def make_choice(options, customer, video, rental):
+    valid_choices = options.keys()
+    choice = None
+
+    while choice not in valid_choices:
+        print("What would you like to do? Select 9 to see all options again")
+        choice = input("Make your selection using the option number: ")
+    
+    # if choice in ['4','5','6','7'] and 
+
+def main(play=True):
+    video = Video(URL)
+    customer = Customer(URL)
+    rental = Rental(URL)
+    options = list_options()
+
+    # while play == True:
+    #     choice
 
 if __name__ == "__main__":
     main()
