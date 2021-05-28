682126f6415663634046e7e75a79a88ee8ea5cf2
Courtney <courtneycmassey@gmail.com>
2021-05-27 22:05:53 -0700

    adds date validation to Option #2

--- main.py
+++ main.py
@@ -26,12 +26,11 @@ def run_cli(play=True):
             if valid_release_date(release_date):
                 release_date = release_date
             else:
-                print(">>> Invalid format. Please enter date as MM-DD-YYYY.")
                 release_date=input(">>> Release Date (MM-DD-YYYY): ")
                 if valid_release_date(release_date):
                     release_date = release_date
                 else:
-                    print(">>> Invalid format again. Release date will default to NONE.")
+                    print(">>> Invalid entry again. Release date will default to NONE.")
                     release_date = None
             total_inventory=input(">>> Total Inventory: ")
             response = video_store.create_video(title=title, release_date=release_date, total_inventory=total_inventory)
@@ -45,21 +44,29 @@ def run_cli(play=True):
             if video == None:
                 print("No video with that ID or title found.")
             else:
-                # potential place to add more elements of choice for user
-                # ask if they want to update each attribute of the movie
                 print(f"Ok, let's edit video #{video['id']}")
+                print(f"Press enter to skip over an attribute.")
                 title=input(">>> Title: ")
                 release_date=input(">>> Release Date (MM-DD-YYYY): ")
+                if valid_release_date(release_date):
+                    release_date = release_date
+                else:
+                    release_date=input(">>> Release Date (MM-DD-YYYY): ")
+                    if valid_release_date(release_date):
+                        release_date = release_date
+                    else:
+                        print(">>> Invalid entry again. Release date will default to NONE.")
+                        release_date = None
                 total_inventory=input(">>> Total Inventory: ")
                 response = video_store.update_video(
                                             title=title, 
                                             release_date=release_date, 
                                             total_inventory=total_inventory)
                 print(f"Updates successfully made to video ID: {response['id']}")
-                print(f">>>Title: {response['title']}")
-                print(f">>>Release Date: {response['release_date']}")
-                print(f">>>Total Inventory: {response['total_inventory']}")
-                print(f">>>Available Inventory: {response['available_inventory']}")
+                print(f">>> Title: {response['title']}")
+                print(f">>> Release Date: {response['release_date']}")
+                print(f">>> Total Inventory: {response['total_inventory']}")
+                print(f">>> Available Inventory: {response['available_inventory']}")
         
         # "3": "Delete a Video"
         elif choice=='3':
@@ -70,9 +77,9 @@ def run_cli(play=True):
                 print("No video with that ID or title found.")
             else:
                 print(f"You selected video #{video['id']} to delete: ")
-                print(f">>>Title: {video['title']}")
-                print(f">>>Release Date: {video['release_date']}")
-                print(f">>>Total Inventory: {video['total_inventory']}")
+                print(f">>> Title: {video['title']}")
+                print(f">>> Release Date: {video['release_date']}")
+                print(f">>> Total Inventory: {video['total_inventory']}")
                 print(f"Are you sure you want to delete?")
                 certainty = input(f"Select Y or N: ")
                 if certainty == "Y":
@@ -95,18 +102,18 @@ def run_cli(play=True):
                 print("No video with that ID or title found.")
             else:
                 print(f"Information for video #{video['id']}:")
-                print(f">>>Title: {video['title']}")
-                print(f">>>Release Date: {video['release_date']}")
-                print(f">>>Total Inventory: {video['total_inventory']}")
-                print(f">>>Available Inventory: {video['available_inventory']}")
+                print(f">>> Title: {video['title']}")
+                print(f">>> Release Date: {video['release_date']}")
+                print(f">>> Total Inventory: {video['total_inventory']}")
+                print(f">>> Available Inventory: {video['available_inventory']}")
 
         #Option 6: Add a Customer
         elif choice=='6':
 
             print("Ok! Let's add a new customer.")
-            name=input("What is the customer's name? ")
-            postal_code=input("What is the customer's postal code? ")
-            phone=input("What is the customer's phone number? ")
+            name=input(">>> Name: ")
+            postal_code=input(">>> Postal Code: ")
+            phone=input(">>> Phone Number: ")
 
             response = video_store.create_customer(name=name, postal_code=postal_code, phone=phone)
             print(f"New customer ID: {response['id']}")
@@ -120,10 +127,8 @@ def run_cli(play=True):
             if customer == None:
                 print("No customer with that ID or name found.")
             else:
-                # potential place to add more elements of choice for user
-                # ask if they want to update each attribute of the customer
-
                 print(f"Ok, let's edit customer #{customer['id']}")
+                print(f"Press enter to skip over an attribute.")
                 name=input(">>> Name: ")
                 postal_code=input(">>> Postal Code: ")
                 phone=input(">>> Phone Number: ")
@@ -133,11 +138,11 @@ def run_cli(play=True):
                                             postal_code=postal_code, 
                                             phone=phone)
                 print(f"Updates successfully made to customer ID: {response['id']}")
-                print(f">>>Name: {response['name']}")
-                print(f">>>Postal Code: {response['postal_code']}")
-                print(f">>>Phone: {response['phone']}")
-                print(f">>>Registered At: {response['registered_at']}")
-                print(f">>>Videos Checked Out: {response['videos_checked_out_count']}")
+                print(f">>> Name: {response['name']}")
+                print(f">>> Postal Code: {response['postal_code']}")
+                print(f">>> Phone: {response['phone']}")
+                print(f">>> Registered At: {response['registered_at']}")
+                print(f">>> Videos Checked Out: {response['videos_checked_out_count']}")
 
         #Option 8: Delete a Customer
         elif choice=='8':
@@ -165,11 +170,11 @@ def run_cli(play=True):
                 print("No customer with that ID or name found.")
             else:
                 print(f"Information for customer #{customer['id']}:")
-                print(f">>>Name: {customer['name']}")
-                print(f">>>Postal Code: {customer['postal_code']}")
-                print(f">>>Phone: {customer['phone']}")
-                print(f">>>Registered At: {customer['registered_at']}")
-                print(f">>>Videos Checked Out: {customer['videos_checked_out_count']}")
+                print(f">>> Name: {customer['name']}")
+                print(f">>> Postal Code: {customer['postal_code']}")
+                print(f">>> Phone: {customer['phone']}")
+                print(f">>> Registered At: {customer['registered_at']}")
+                print(f">>> Videos Checked Out: {customer['videos_checked_out_count']}")
 
         #Option 10: Get info for ALL Customers
         elif choice=='10':
@@ -246,12 +251,12 @@ def list_options():
         "2": "Edit a Video",
         "3": "Delete a Video",
         "4": "Get all Videos",
-        "5": "Get One Video",
+        "5": "Get one Video",
         "6": "Add a Customer",
         "7": "Edit a Customer",
         "8": "Delete a Customer",
-        "9": "Get Customer Info for One Customer",
-        "10": "Get Customer Info for All Customers",
+        "9": "Get one Customerr",
+        "10": "Get all Customers",
         "11": "Check OUT a Video",
         "12": "Check IN a Video",
         "13": "QUIT"
@@ -331,7 +336,7 @@ def valid_release_date(date_string):
         datetime.datetime.strptime(date_string, format)
         return True 
     except ValueError:
-        print("This is the incorrect date string format. It should be MM-DD-YYYY")
+        print(">>> Invalid entry. Please enter date as MM-DD-YYYY")
         return False
 
 
