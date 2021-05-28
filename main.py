3cd93628cf3779f4a21c7af8cd315fb16b068f67
alison <alimcginley@gmail.com>
2021-05-27 18:19:24 -0700

    adds comments

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ URL = "http://127.0.0.1:5000"
 BACKUP_URL = "https://localhost:5000"
 
 def list_options():
+###LIST ALL OPTIONS
     options = {
         "1" : "Add a Video", 
         "2" : "Select a Video",
@@ -26,6 +27,7 @@ def list_options():
     return options
 
 def make_choice(all_options, video, customer, message = None, choice = None):
+###MAKE AND VALIDATE CHOICE
     valid_choices = all_options.keys()
 
     while choice not in valid_choices:
@@ -35,7 +37,7 @@ def make_choice(all_options, video, customer, message = None, choice = None):
     return choice
 
 def main(store_open=True):
-
+###INITIALIZE CLASS INSTANCES
     video = Video(URL)
     customer = Customer(URL)
     rental = Rental(URL)
@@ -43,7 +45,8 @@ def main(store_open=True):
 
     while store_open:
         choice = make_choice(options, video, customer)
-        
+ 
+ ###ADD A VIDEO (1)      
         if choice == '1':
             print("Let's add a video!")
             title = input("What's the name of the video?")
@@ -52,7 +55,8 @@ def main(store_open=True):
             response = video.add_video(title=title, release_date=release_date, total_inventory=total_inventory)
 
             print("New Video:", response)
-        
+ 
+ ###SELECT A VIDEO (2)       
         elif choice == '2':
             select_by = input("Would you like to select by title, id, or release date?")
             if select_by == "title":
@@ -72,10 +76,12 @@ def main(store_open=True):
             if video.selected_video:
                 next_step = input("What would you like to do with the video? Select 1 to delete, or 2 to edit.")
 
+###DELETE SELECTED VIDEO
                 if next_step == '1':
                     print(f"Video {video.selected_video['title']} is being deleted.")
                     video.delete_video()
-                
+
+###UPDATE SELECTED VIDEO               
                 elif next_step == '2':
                     print(f"Great! Let's edit {video.selected_video['title']}")
                     update = input("Would you like to edit the title, release date, or inventory?")
@@ -88,11 +94,12 @@ def main(store_open=True):
                     response = video.update_video(title, release_date, total_inventory)
 
                     print("Updated video:", response["video"])
-                
+ 
+ ###LIST ALL VIDEOS (3)       
         elif choice == '3':
             print(video.list_videos())
         
-        
+###ADD A CUSTOMER (4)       
         elif choice == '4':
             print("Let's add a new customer!")
             name = input("What's the name of the customer?")
@@ -101,7 +108,8 @@ def main(store_open=True):
 
             response = customer.add_customer(name=name, phone=phone, postal_code=postal_code)
             print("New customer:" , response["name"])
-        
+
+###SELECT A CUSTOMER(5)        
         elif choice == '5':
             select_by = input("Would you like to find the customer by name, phone, or id?")
             if select_by == 'name':
@@ -120,14 +128,17 @@ def main(store_open=True):
                 print("Please enter name, phone, or id")
         
             customer.print_selected()
-            
+
+###NEXT STEPS FOR SELECTED CUSTOMER            
             if customer.selected_customer:
                 next_step = input(f"How can I help you with {customer.selected_customer['name']}? \nSelect 1 to Delete, or 2 to Edit.")
-                
+
+###DELETE SELECTED CUSTOMER                
                 if next_step == "1":
                     print(f"Customer {customer.selected_customer['name']} is being deleted.")
                     customer.delete_customer()
-                
+ 
+ ###UPDATE SELECTED CUSTOMER               
                 elif next_step == "2":
                     print("Great! Let's update the customer!")
                     update = input("Would you like to update the name, phone, or postal number?")
@@ -139,27 +150,32 @@ def main(store_open=True):
                         postal_code = input("what is the updated postal code?")
             
                     response = customer.update_customer(name, phone, postal_code)
-        
+ 
+ ###LIST ALL CUSTOMERS (6)     
         elif choice == '6':
             print(customer.list_customers())
-        
+
+ ###CHECK OUT A RENTAL (7)      
         elif choice == '7':
             customer_id = input("Which customer id is renting?")
             video_id = input("Which video id are they renting?")
             response = rental.check_out(int(customer_id), int(video_id))
-            
+
             print("Enjoy the movie!")
-        
+
+###CHECK IN A RENTAL (8)        
         elif choice == '8':
             customer_id = input("Which customer id is returning?")
             video_id = input("Which video id are they returning?")
             response = rental.check_in(int(customer_id), int(video_id))
             
-            print("Thanks for bringing it back!")
+            return response
 
+###LIST OPTIONS (0)
         elif choice == '0':
             list_options()
-        
+
+###CLOSE THE STORE (99)        
         elif choice == "99":
             store_open = False
             print("Don't forget to lock up on your way out!")
