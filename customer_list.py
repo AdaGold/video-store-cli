08ca9b1c3db102d9892b0c80972f3e59ec8640cb
Sara <sarajessvega17@gmail.com>
2021-05-28 12:00:05 -0500

    Minimum requirements met. CRUD customers. CRUD videos. List all customers and videos. Check in/out video.

---
+++ customer_list.py
@@ -0,0 +1,59 @@
+import requests
+import datetime
+from requests.api import post
+
+from requests.models import Response
+
+class CustomerList:
+    def __init__(self, url='https://retro-video-store-api.herokuapp.com', selected_customer=None):
+        self.url = url
+        self.selected_customer = selected_customer
+    
+    def get_customer(self, name=None, id=None):
+        for customer in self.list_all_customers():
+            if customer["id"] == id:
+                self.selected_customer = customer
+
+        response = requests.get(self.url+f"/customers/{id}")
+        return response.json()
+
+    def create_customer(self, name="default", postal_code="default", phone="default"):
+        query_params = {
+            "name": name,
+            "postal_code": postal_code,
+            "phone": phone
+        }
+        response = requests.post(self.url+"/customers", json=query_params )
+        return response.json()
+
+    def update_customer(self, name=None, postal_code=None, phone=None):
+        query_params = {
+        "name": name,
+        "postal_code": postal_code,
+        "phone": phone
+        }
+        response = requests.put(
+            self.url+f"/customers/{self.selected_customer['id']}",
+            json=query_params
+            )
+        print("response:", response.json())
+        # self.selected_task = response.json()["customer"]
+        return response.json()
+
+    def list_all_customers(self):
+        response = requests.get(self.url +"/customers")
+        return response.json()
+        
+    def delete_customer(self, id=None):
+        response = requests.delete(self.url +f"/customers/{self.selected_customer['id']}")
+        return response.json()
+
+    def check_out_video():
+        pass
+
+    def check_in_video():
+        pass
+    
+    def print_selected(self):
+        if self.selected_customer:
+            print(f"Customer with id {self.selected_customer['id']} and name {self.selected_customer['name']} selected!")
