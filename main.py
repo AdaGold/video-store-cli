0ec3cd663166e08bb7bdface7824718567c9f2fa
Laurel <lolkinetzky@gmail.com>
2021-05-28 20:26:51 -0700

    completed requirements 1-5 chck/in/out in works

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import requests
+from video_store import Video_store
 
-URL = "http://127.0.0.1:5000"
-BACKUP_URL = "https://retro-video-store-api.herokuapp.com"
+#URL = "http://localhost:5000"
+URL = "https://retro-video-store-api.herokuapp.com"
 
 def main():
     print("WELCOME TO RETRO VIDEO STORE")
@@ -10,3 +11,149 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+def print_stars():
+    print("\n**************************\n")
+
+def list_options():
+
+    options = {
+        "1": "List all videos in stock", 
+        "2": "Create a new video record",
+        "3": "Select a video", 
+        "4": "Update selected video", 
+        "5": "Delete selected video", 
+        "6": "Delete all videos in stock",
+        "7": "List all options",
+        "8": "Quit",
+        "9": "Rent out a video",
+        "10": "Return a video"
+        }
+
+    print_stars()
+    print("Welcome")
+    print("These are the actions you can perform")
+    print_stars()
+    
+    for choice_num in options:
+        print(f"Option {choice_num}. {options[choice_num]}")
+
+    print_stars()
+
+    return options
+
+
+def make_choice(options, video_store):
+    valid_choices = options.keys()
+    choice = None
+
+    while choice not in valid_choices:
+        print("What would you like to do? Select 7 to see all options again")
+        choice = input("Make your selection using the option number: ")
+
+    if choice in ['4','5'] and video_store.selected_video == None:
+        print("You must select a video before updating it, deleting it, checking it out, or checking back in.")
+        print("Let's select a video!")
+        choice = "3"
+    
+    return choice
+
+def run_cli(play=True):
+
+    #initialize video
+    video_store = Video_store(URL) ##put url here if you can't find it ya silly
+    
+    # print choices
+    options = list_options()
+
+    while play==True:
+
+        # get input and validate
+        choice = make_choice(options, video_store)
+
+        #video_store.print_selected()
+
+        if choice=='1':
+            print_stars()
+            for video in video_store.list_videos():
+                print(video)
+
+        elif choice=='2':
+            print("Great! Let's create a new video.")
+            title=input("What is the name of the video? ")
+            release_date=input("When was this movie released?")
+            total_inventory=input("How many copies of this video are we adding?")
+            response = video_store.create_video(title=title, release_date=release_date, total_inventory= total_inventory)
+
+            print_stars()
+            print("New video:", response)
+
+        elif choice=='3':
+            select_by = input("Would you like to select by? Enter title or id: ")
+            if select_by=="title":
+                title = input("Which title would you like to select? ")
+                video_store.get_video(title=title)
+            elif select_by=="id":
+                video_id = input("Which video id would you like to select? ")
+                if video_id.isnumeric():
+                    video_id = int(video_id)
+                    video_store.get_video(video_id=video_id)
+            else:
+                print("Could not select. Please enter id or title.")
+            
+            if video_store.selected_video:
+                print_stars()
+                print("Selected video: ", video_store.selected_video)
+        
+        elif choice=='4':
+            print(f"Great! Let's update the movie: {video_store.selected_video}")
+            title=input("What is the title of your new video? ")
+            release_date=input("When was it released?")
+            total_inventory=input("How many copies do you have available to add to stock?")
+            response = video_store.update_video(title=title, release_date=release_date, total_inventory=total_inventory)
+
+            print_stars()
+            print("video:", response)
+        elif choice=='5':
+            video_store.delete_video()
+
+            print_stars()
+            print("Movie has been deleted.")
+
+            print_stars()
+
+        elif choice=='6':
+            for video in video_store.list_videos():
+                video_store.get_video(video_id=video['id'])
+                video_store.delete_video()
+
+            print_stars()
+            print("Deleted all videos in stock. Out with the old.")
+
+        elif choice=='7':
+            list_options()
+
+        elif choice=='8':
+            play=False
+            print("\nThanks!")
+
+        elif choice =='9':
+            customer_id = input("Which customer is renting today? (please provide customer id)")
+            video_id = input("Which video would they like to rent? (please provide video id)" )
+            response = video_store.check_out_video(int(customer_id), int(video_id))
+            print(response) 
+            print_stars()
+
+        elif choice =='10':
+            customer_id = input("Which customer is returning today? (please provide customer id)")
+            video_id = input("Which video are they returning? (please provide video id)")
+            response = video_store.check_in_video(int(customer_id), int(video_id))
+            print(response) 
+            print_stars()
+
+
+        print_stars()
+
+
+run_cli()
+
---
+++ video_store.py
@@ -0,0 +1,107 @@
+import requests
+import datetime
+from flask import json
+
+
+##could also put a helper function for select video in main.py
+# 
+#  
+###below is becc'as code
+class Video_store:
+    def __init__(self, URL = "https://retro-video-store-api.herokuapp.com", selected_video=None, selected_customer=None): #selected_customer=None, selected_rental=None ):
+        self.url = URL
+        self.selected_video = selected_video
+        self.selected_customer = selected_customer
+
+
+    ########## video actions to do: edit, still need to decide which query params needed and how to edit (look at routes?)
+    ##Check out video to customer: part of video requirements is to be able to check it out to customers, which is through rental routes
+    def check_out_video(self, video_id, customer_id):
+        query_params={
+            "customer_id": customer_id,
+            "video_id": video_id
+        }
+        response = requests.post(self.url+"/rentals/check-out", json = query_params)
+        return response
+
+    def check_in_video(self, video_id, customer_id):
+        query_params={
+            "customer_id": customer_id,
+            "video_id": video_id
+        }
+        response = requests.post(self.url+"/rentals/check-in", json= query_params)
+        return response
+
+    def print_selected(self):
+        pass
+        if self.selected_video:
+            print(f"Video with id {self.selected_video} is selected.") 
+        else:
+            print("Video not found")
+
+    def create_video(self, title= "Default title", release_date= "Default release date", total_inventory= "default stock"):
+
+        query_params = {
+            "title": title,
+            "release_date": release_date,
+            "total_inventory": total_inventory,
+        }
+        response = requests.post(self.url+"/videos",json=query_params)
+        return response.json()
+
+    def list_videos(self): #List videos, get information about all videos  | I can see the store stock  |
+        response = requests.get(self.url+"/videos")
+        return response.json()
+
+    #| List videos #2 get information about one video  | I can see how many copies
+    def get_video(self, title=None, video_id=None, release_date= None):
+        
+        for video in self.list_videos():
+            if title:
+                if video["title"]==title:
+                    video_id = video["id"]
+                    self.selected_video = video
+            elif int(video_id) == video["id"]:
+                self.selected_video = video
+            # elif release_date:
+            #     if video["release_date"]==release_date:
+            #         self.selected_video = video 
+
+        if self.selected_video == None:
+            return "Could not find video in stock"
+
+        response = requests.get(self.url+f"/videos/{video_id}") #self. instead of requests?
+        return response.json()
+
+    #edit a video  | the information about the video is accurate
+    def update_video(self,title=None, video_id=None, release_date=None, total_inventory=None): 
+        if not title:
+            title = self.selected_video["title"]
+        if not video_id:
+            video_id = self.selected_video["id"]
+        if not release_date:
+            release_date = self.selected_video["release_date"]
+        if not total_inventory:
+            total_inventory = self.selected_video["total_inventory"]
+
+        query_params = {
+            "id": video_id,
+            "title": title,
+            "release_date": release_date,
+            "total_inventory": total_inventory}
+        
+        response = requests.put(
+            self.url+f"/videos/{self.selected_video['id']}",
+            json=query_params
+            )
+        print("response:", response) #add ansi color codes here?
+        self.selected_video = response.json()
+        return response.json()
+
+    # delete a video  | the store records stay up to date  |
+    def delete_video(self): #delete a video
+        response = requests.delete(self.url+f"/videos/{self.selected_video['id']}")
+        self.selected_video = None
+        return response.json()
+    #################################################### end video actions
+    
