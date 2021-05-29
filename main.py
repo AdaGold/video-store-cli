274fc495b90262165b2fc840c3111d496792cbe4
halisa <halisasaipulaa@gmail.com>
2021-05-28 18:33:10 -0700

    final

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-# import requests
-
-# URL = "http://127.0.0.1:5000"
-# BACKUP_URL = "https://retro-video-store-api.herokuapp.com"
-
-# def main():
-#     print("WELCOME TO RETRO VIDEO STORE")
-#     pass
-
-
-# if __name__ == "__main__":
-#     main()
-
-
 from check_in_check_out_list import CheckInOut
 from requests.api import options
 from video_list import VideoList
