1e5b3a9b982d2d9004a97267a7d4eb057f476c33
Lauren <lacovington16@gmail.com>
2021-05-28 20:49:57 -0700

    draft for submission, plan to finish post-due-date

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-from video_store import VideoStore # import class + instance methods from other file
+from video_store import VideoStore 
 
 URL = "http://127.0.0.1:5000"
 BACKUP_URL = "https://retro-video-store-api.herokuapp.com"
@@ -7,8 +7,7 @@ BACKUP_URL = "https://retro-video-store-api.herokuapp.com"
 def mark_intro():
     print("\n**************************\n")
 
-
-def list_menu_options(): # DISPLAYS OPTIONS TO BLOCKBUSTER EMPLOYEE
+def list_menu_options(): 
     menu = {
         "1": "List all customers in Blockbuster's database", 
         "2": "Create a customer account in the Blockbuster database",
@@ -19,11 +18,11 @@ def list_menu_options(): # DISPLAYS OPTIONS TO BLOCKBUSTER EMPLOYEE
         "6": "List all videos in Blockbuster's database", 
         "7": "Add a video to the Blockbuster database",
         "8": "Select a video from the database", 
-        "9": "Update a video's due date", # any other attrs that can be updated for video? 
+        "9": "Update a video's due date",
         "10": "Delete a video from the database",
 
-        "11": "Check out a video to a customer", # checkout
-        "12": "Check in a video from a customer", # check in
+        "11": "Check out a video to a customer", 
+        "12": "Check in a video from a customer",
 
         "13": "List all menu options",
         "14": "Quit"
@@ -32,144 +31,139 @@ def list_menu_options(): # DISPLAYS OPTIONS TO BLOCKBUSTER EMPLOYEE
     mark_intro()
     print("WELCOME TO BLOCKBUSTER'S DATABASE\n")
     print("Below is a list of actions you can take: \n")
-    for option in menu: # for every key in the dict
+    for option in menu: 
         print(f"Option {option}. {menu[option]}")
     print()
     return menu
 
 def select_action(menu, video_store):
-    choices_offered = menu.keys() # "1", "2", etc.
-    employee_choice = None # ini at falsey to start while loop
+    choices_offered = menu.keys() 
+    employee_choice = None
 
     while employee_choice not in choices_offered:
         print("What action are you looking to perform? ")
         employee_choice = input("Use your keypad to make a selection: ")
-    if employee_choice in ["4", "5"] and video_store.selected_customer == None: # 11 and 12 too? add if statement for 'video must exist'?
+    if employee_choice in ["4", "5"] and video_store.selected_customer == None: 
         print("You have to select a customer account that exists before performing any work on it.")
-        print("Choose a customer. ")
+        print("Choose a customer by pressing 3. ")
         employee_choice = 3
-    return employee_choice # returns a string data type choice number
+    return employee_choice 
 
 def run_cli(play=True):
-    video_store = VideoStore(url="http://127.0.0.1:5000")
-    choices = list_menu_options() # dict of all menu options
+    video_store = VideoStore(url="https://retro-video-store-api.herokuapp.com")
+    choices = list_menu_options() 
 
     while play == True:
-        employee_choice = select_action(choices, video_store) # str
-        if employee_choice in ["1","2","3","4","5"]: # 2 not working
+        employee_choice = select_action(choices, video_store) 
+        if employee_choice in ["1","2","3","4","5"]: 
             video_store.show_selected_customer()
         elif employee_choice in ["6","7","8","9","10"]:
             video_store.show_selected_video()
         
-        if employee_choice == '1': # if they ask to see all customers
+        if employee_choice == '1':
             mark_intro()
-            mark_intro()
-            for customer in video_store.list_customers():
-                print(customer)
-        elif employee_choice == '2':
+            for customer in video_store.list_customers(): 
+                print(customer) 
+        elif employee_choice == '2': 
             print("We'll create a new customer account, then. ")
             name = input("Enter the customer's name: ")
             phone = input("Enter the customer's phone number: ")
             postal_code = input("Enter the customer's postal code: ")
-            for_display = video_store.create_customer(name=name, phone=phone, postal_code=postal_code)
+            for_display = video_store.create_customer(name=name, phone=phone, postal_code=postal_code) 
             mark_intro()
-            print("Customer account info: ", for_display["customer"]) # create_customer() called on 74 should return a jsonified dict whose var name is "customer"
+            print('FOR DISPLAY: ', for_display) # {'id': 116}
+            print("Customer created. Their account ID is: ", for_display["id"])
         elif employee_choice == '3':
-            choice_param = input("Want to select a customer by name, phone number or ID? ")
-            if choice_param == "name":
+            choice_param = input("Want to select a customer by name or ID? ")
+            if choice_param == "name": 
                 name = input("Enter the name of the customer whose account you want to work with: ")
-                video_store.get_customer(name=name)
-            elif choice_param == "phone number":
-                phone = input("Enter the phone number of the customer account you want to work with: ")
-                video_store.get_customer(phone=phone)
-            # IF MAKING CLI MY WAY, DO I EVEN NEED TO ALLOW ID-LOOKUP? WHAT'S SIMPLEST?
-            elif choice_param == "id": # address case issue?
+                print("Customer account info you selected: ", video_store.get_customer(name=name))
+            elif choice_param == "id":
+                # print("LOOOOOK: ", type(video_store.selected_customer)) # <class 'NoneType'>
                 cli_id = input("Which customer ID would you like to select? ")
-                if cli_id.isnumeric(): # handle id coming in as str (from HTTP path?)
+
+                if (cli_id.isnumeric()) == False:
+                    print("Use digits, please.")
+                elif (cli_id.isnumeric()) == True:
                     cli_id = int(cli_id)
-                    video_store.get_customer(cli_id=video_store.customer_id) # WHAT SHOULD CUSTOMER_ID BE? TO BE RECOGNIZED?
+                    print("Customer account info you selected: ", video_store.get_customer(id=cli_id))
             else:
-                print("Could not select. Please enter the name or phone number of an existing customer. ")
-            
-            if video_store.selected_customer:
-                mark_intro()
-                print("Here is the customer you selected: ", video_store.selected_customer)
+                print("Could not select. Please enter the name or ID of an existing customer. ")
         elif employee_choice == '4': 
-            print(f"Let's update the account information for {video_store.selected_customer}, then. ")
+            print(f"Then, let's update the account information inside this collection: {video_store.selected_customer}. ")
             name = input("Enter the new name for the customer account: ")
             phone = input("Enter the customer's new phone number: ")
             postal_code = input("Enter the customer's new postal code: ")
             for_update = video_store.update_customer(name=name, phone=phone, postal_code=postal_code)
-
             mark_intro()
-            print("Here's the new account information: ", for_update["customer"]) # see choice 2 final lines for explanation
+            print("Here's the new account information: ", for_update) # Here's the new account information: {'id': 198, 'name': 'Maxine Waters', 
+                                                                        # 'phone': '333-333-0000', 'postal_code': '20022',
+                                                                        # 'registered_at': 'Sat, 29 May 2021 01:56:21 GMT', 'videos_checked_out_count': 0}
         elif employee_choice == '5':
             video_store.delete_customer()
             mark_intro()
-            print("This customer account has been deleted. ")
+            print("This customer account has been deleted. See below: ")
             mark_intro()
-            print(video_store.list_customers()) # confirm that customer's no longer there
-
-
-        elif employee_choice == '6': # if they ask to see all videos
-            mark_intro()
+            for customer in video_store.list_customers():
+                print(customer)
+        elif employee_choice == '6':
             mark_intro()
             for video in video_store.list_videos():
                 print(video)
         elif employee_choice == '7':
             print("We'll add a new video to the database, then. ")
             title = input("Enter the video's title: ")
-            release_date = input("Enter the video's release date: ") # account for formatting differences?
+            release_date = input("Enter the video's release date (format: YYYY-MM-DD): ")
             total_inventory = input("Enter the number of copies that'll be added to the stock: ")
-            available_inventory = total_inventory # collection of video copies just go to the store: none are checked out yet
+            available_inventory = total_inventory
 
-            for_display = video_store.create_video(title=title, release_date=release_date, total_inventory=total_inventory, available_inventory=available_inventory)
+            for_display = video_store.create_video(title=title, release_date=release_date, total_inventory=total_inventory,\
+                                                    available_inventory=available_inventory)
             mark_intro()
-            print("Video addition details: ", for_display["video"]) # create_video() called on 120 should return a jsonified dict whose var name is "video"
+            print("Video addition details: ", for_display) # {'id': 238}
         elif employee_choice == '8':
             choice_param = input("Want to select a video by title or ID? ")
             if choice_param == "title":
-                name = input("Enter the title of the video you want to work with: ")
-                video_store.get_video(title=title)
-            # IF MAKING CLI MY WAY, DO I EVEN NEED TO ALLOW ID-LOOKUP? WHAT'S SIMPLEST?
-            elif choice_param == "id": # address case issue?
+                title = input("Enter the title of the video you want to work with: ")
+                print("Video info you selected: ", video_store.get_video(title=title))
+            elif choice_param == "id": # address case issue later
                 cli_id = input("Which video ID would you like to select? ")
-                if cli_id.isnumeric(): # handle id coming in as str (from HTTP path?)
+                if (cli_id.isnumeric()) == False:
+                    print("Use digits, please.")
+                elif (cli_id.isnumeric()) == True:
                     cli_id = int(cli_id)
-                    video_store.get_video(cli_id=video_store.video_id) # WHAT SHOULD VIDEO_ID BE? TO BE RECOGNIZED?
+                    print("Video info you selected: ", video_store.get_video(id=cli_id)) # ... = {'id': 237, 'release_date': 'Sun, 12 May 2002 00:00:00 GMT', 
+                                                                                        # 'title': 'The Matrix', 'total_inventory': 5}
             else:
-                print("Could not select. Please enter the title or ID of a video that's been added to the database. ")
-            
-            if video_store.selected_video:
-                mark_intro()
-                print("Here is the video you selected: ", video_store.selected_video)
-        elif employee_choice == '9': 
-            print(f"Let's update the due date for {video_store.selected_video}, then. ")
-            due_date = input("Enter the new due date for the video using format YYYY-MM-DD: ") # define and control for format???????
-            for_update = video_store.update_rental(due_date=due_date) # proper param??
-
+                print("Could not select. Please enter the title or ID of an existing video. ")
+        elif employee_choice == '9': # NOT WORKING 5/27/21, 8:27pm  
+            print(f"Then, let's update the due date inside this collection: {video_store.selected_video}. ")
+            due_date = input("Enter the new due date for the video using format YYYY-MM-DD: ") 
+            for_update = video_store.update_rental(due_date=due_date) # ERROR THROWN HERE. linked to line 107 in video_store.py
+            print("HEYOOOOO: ", for_update)
             mark_intro()
-            print("Here's the video's new due date: ", for_update["rental"]) # see choice 2 final lines for explanation; verify dict format: does that give the date itself?
+            print("Here's the video's new due date: ", for_update) # should be {"due_date": whatever the given date format is}
         elif employee_choice == '10':
             video_store.delete_video()
             mark_intro()
-            print("This video has been deleted from the database. ")
+            print("This video has been deleted from the database. See below: ")
             mark_intro()
-            print(video_store.list_videos()) # confirm that customer's no longer there
-        elif employee_choice == '11':
-            pass # CHECK-OUT PROCESS
-        elif employee_choice == '12':
-            pass # CHECK-IN PROCESS
+            for video in video_store.list_videos():
+                print(video)
+        elif employee_choice == '11': # CHECK-OUT PROCESS; call check_out func from video_store.py
+            pass 
+        elif employee_choice == '12': # CHECK-IN PROCESS; call check_out func from video_store.py
+            pass
         elif employee_choice == '13':
             list_menu_options()
         elif employee_choice == '14':
-            play=False # set flag to False to stop the while loop
-            print("\nThanks for using the Video Store CLI. Byeeeeee! ") # exit msg
-        mark_intro() # make sure this is in while loop
-        mark_intro() # "" 
+            play=False
+            print("\nThanks for using the Video Store CLI. Byeeeeee! ")
+        mark_intro()
+
 
 run_cli()
 
-# NEED THIS OR NO? change 'main' to '__run_cli__' and run_cli() ? 
+# change 'main' to '__run_cli__' and main() to run_cli() ? 
 #if __name__ == "__main__": # func holding menu options dict used to be called main()
 #   main()
--- video_store.py
+++ video_store.py
@@ -1,15 +1,23 @@
 import requests
 import datetime
 
-class VideoStore: # how many params for __init__? selected_video and selected_customer? so you can choose to work via either entity?
-    def __init__(self, url="http://localhost:5000", selected_video=None, selected_customer=None, selected_rental=None): # url is for local machine... do you even need to deploy to Heroku??
+# - Customer create, edit, delete  --- check
+# - Video create, edit, delete  --- check 
+# - List customers --- check
+# - List videos --- check
+# - Check out video to customer --- *missing
+# - Check in video  --- *missing
+
+class VideoStore:
+    def __init__(self, url="https://retro-video-store-api.herokuapp.com", selected_video=None, selected_customer=None, selected_rental=None):
         self.url = url
         self.selected_video = selected_video
         self.selected_customer = selected_customer
-        self.selected_rental = selected_rental # is this right? or operate through the video model?
+        self.selected_rental = selected_rental # operate through this attribute or video?
 
-    def create_customer(self, name="Default Customer Name", postal_code="Default Postal Code", phone="Default Phone Number", register_at=datetime.datetime.now(), videos_checked_out_count=0): # what other params? all the ones that make up the customer object? just the ones the blockbuster emp can work on?
-        relevant_params = { # new customer must have an account; blockbuster employee adds them to the system
+    def create_customer(self, name="Default Customer Name", postal_code="Default Postal Code", phone="Default Phone Number",\
+                        register_at=str(datetime.datetime.now()), videos_checked_out_count=0): 
+        relevant_params = {
             "name": name,
             "postal_code": postal_code,
             "phone": phone,
@@ -17,10 +25,11 @@ class VideoStore: # how many params for __init__? selected_video and selected_cu
             "videos_checked_out_count": videos_checked_out_count
         }
         response = requests.post(self.url + "/customers", json=relevant_params)
-        return response.json()
+        return response.json() # example return value: {'id': 116}
 
-    def create_video(self, title="Default Movie Title", release_date="Default Date", total_inventory="Default Total Inventory", available_inventory="Default Available Inventory"): # add video to database
-        relevant_params = { # blockbuster employee has to add new movie to bb system for potental rent-out
+    def create_video(self, title="Default Movie Title", release_date="Default Date", total_inventory="Default Total Inventory",\
+                    available_inventory="Default Available Inventory"):
+        relevant_params = {
             "title": title,
             "release_date": release_date,
             "total_inventory": total_inventory,
@@ -29,102 +38,110 @@ class VideoStore: # how many params for __init__? selected_video and selected_cu
         response = requests.post(self.url + "/videos", json=relevant_params)
         return response.json()
 
-    def list_customers(self): # show all customers in blockbuster db
+    def list_customers(self):
         response = requests.get(self.url + "/customers")
         return response.json()
 
-    def list_videos(self): # show all movies blockbuster has to offer
+    def list_videos(self):
         response = requests.get(self.url + "/videos")
         return response.json() 
 
-    def get_customer(self, name=None, id=None): # pull up specific customer acct, by name, phone number or customer id(would a customer even have that info? phone num's prob best)
+    def get_customer(self, name=None, id=None):
         for customer in self.list_customers():
-            if customer:
+            if customer: 
                 if customer["name"] == name:
-                    id = customer["customer_id"]
+                    # customer = {'id': 136, 'name': 'Ada Lovelace', 'phone': '333-333-3333', 'postal_code': '88890',
+                                # 'registered_at': 'Fri, 28 May 2021 20:13:32 GMT', 'videos_checked_out_count': 1}
+                    id = customer["id"]
                     self.selected_customer = customer
-            elif id == customer["customer_id"]:
-                self.selected_customer = customer
-
+                elif customer["id"] == id:
+                    name = customer["name"]
+                    self.selected_customer = customer
         if self.selected_customer == None:
             return "Could not find a customer by that name or ID."
-        
+
         response = requests.get(self.url + f"/customers/{id}")
         return response.json()
 
-    def get_video(self, title=None, id=None): # pull up a video by its title or ID in the system (likely to see if it's been checked out or to refresh customer on when it's due)
+    def get_video(self, title=None, id=None):
         for video in self.list_videos():
             if video:
                 if video["title"] == title:
-                    id = video["video_id"]
+                    id = video["id"]
                     self.selected_video = video
-            elif id == video["video_id"]:
-                self.selected_video = video
-
+                elif video["id"] == id:
+                    title = video["title"]
+                    self.selected_video = video
         if self.selected_video == None:
             return "Could not find a video by that title or ID."
-        
         response = requests.get(self.url + f"/videos/{id}")
         return response.json()
 
-    def update_customer(self, name=None, postal_code=None, phone=None): # 'phone' or 'phone_number'??? --- > params should be what can be updated by user
-    # maybe customer renews it to keep it for longer and the due date field needs to change as a result
-        if not name: # customer's phone number or postal code or name changed; update system per their request
-            name = self.selected_customer["name"] # if employee doenst change name, maintain original
+    def update_customer(self, name=None, postal_code=None, phone=None): 
+        if not name: 
+            name = self.selected_customer["name"]
         if not postal_code:
             postal_code = self.selected_customer["postal_code"]
         if not phone:
-            phone = self.selected_customer["phone_number"] # phone_number or phone?
+            phone = self.selected_customer["phone_number"]
         
         relevant_params = {
             "name": name,
             "postal_code": postal_code,
-            "phone": phone # phone or phone_number?
+            "phone": phone
         }
 
         response = requests.put(
-            self.url + f"/customers/{self.selected_customer['customer_id']}",
+            self.url + f"/customers/{self.selected_customer['id']}",
             json=relevant_params)
-        print("Here's what we have now: ", response)
-        self.selected_customer = response.json()["customer"] # right param?
+        self.selected_customer = response.json()
+        print("Here's what we have now: ", self.selected_customer) # self.selected_customer = {'id': 198, 'name': 'Maxine Waters', 'phone': '222-333-4444', 
+                                                                    # 'postal_code': '90211', 'registered_at': 'Sat, 29 May 2021 01:56:21 GMT',
+                                                                    # 'videos_checked_out_count': 0}
         return response.json()
 
-    def update_rental(self, due_date=None): # "" ******** WORK ON RENTAL MODEL, NOT VIDEO (??)
-        if not due_date: # customer's phone number or postal code or name changed; update system per their request
-            due_date = (self.selected_rental["check_out_date"] + (datetime.timedelta(days=7)))# if employee doenst change due date, maintain original
-        
+    def update_rental(self, due_date=None): # Work on rental instance, not video(?)
+        if not due_date:
+            due_date = (self.selected_rental["check_out_date"] + (datetime.timedelta(days=7)))
         relevant_param = {"due_date": due_date}
-# THE CORRECT LOGIC HERE DEPENDS ON IF WORKING THROUGH RENTAL OR VIDEO
-        response = requests.put(
-            self.url + f"/customers/{self.selected_customer['customer_id']}/rentals", # look up rentals asso'd w X customer, change their due date: right path ????
-            json=relevant_param)
+        response = requests.put(self.url + f"/customers/{self.selected_customer['id']}/rentals", # issue is here: TypeError: 'NoneType' object is not subscriptable
+                                json=relevant_param)
+        # can't see any of this bc path above is wrong
         print("Here's what we have now: ", response)
-        self.selected_rental = response.json()["rental"] # right param?
+        self.selected_rental = response.json()
         return response.json()
-        
 
     def delete_customer(self):
-        response = requests.delete(self.url + f"customers/{self.selected_customer['id']}")
-        self.selected_customer = None # reassign to falsey value = deletion
+        response = requests.delete(self.url + f"/customers/{self.selected_customer['id']}")
+        self.selected_customer = None
         return response.json()
 
     def delete_video(self):
-        response = requests.delete(self.url + f"videos/{self.selected_video['id']}")
-        self.selected_video = None # reassign to falsey value = deletion
+        response = requests.delete(self.url + f"/videos/{self.selected_video['id']}")
+        self.selected_video = None
         return response.json()
-    
-    def delete_rental(self): # don't need this right? bc why would you delete a rental from a customer's account? (XXX? video in wrong box at checkout? sounds like getting into the weeds)
-        pass
 
-    def show_selected_customer(self): # ""
-        # print "Let's confirm that you want to work with Customer X" to the blockbuster employee user
+    def show_selected_customer(self):
         if self.selected_customer:
             print(f"Customer with ID {self.selected_customer['id']} is currently selected\n")
 
-    def show_selected_video(self): # ""
-        # print "Let's confirm that you want to work with Video X" to the blockbuster employee user
+    def show_selected_video(self): 
         if self.selected_video:
             print(f"Video with ID {self.selected_video['id']} is currently selected\n")
+    
+    # incomplete/incorrect logic 5/27/21, 9:03pm
+    def check_out(self, customer_id=None, video_id=None, check_out_date=None):
+        relevant_params = {
+            "customer_id": customer_id,
+            "video_id": video_id,
+            "check_out_date": check_out_date
+        }
+        if self.selected_customer == None:
+            return "Could not find a customer by that name or ID."
+        if self.selected_video == None:
+            return "Could not find a video by that title or ID."
+        response = requests.post(self.url + "/rentals/check-out", json=relevant_params)
+        return response.json()
 
-    def show_selected_rental(self): # dont need to work w this bc no menu option
+    def check_in(self):
         pass
