b0015ee0f66d2d5b62fc3ac1bf6c355064c8bc2b
Kaylyn Cardella <kaylyncardella@gmail.com>
2021-05-28 18:52:14 -0700

    Complete 12/12 base functionality req.

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def menu_options():
     menu_options = {
         "1": "Video Check-Out",
         "2": "Video Check-In",
-        "3": "Video Inventory",
+        "3": "Video Services",
         "4": "Customer Services"
     }
 
@@ -30,7 +30,7 @@ def menu_options():
 
     for option in menu_options:
         print(f"Option {option}. {menu_options[option]}")
-    
+    print()
     return menu_options
 
 def video_options():
@@ -49,7 +49,8 @@ def video_options():
 
     for option in video_options:
         print(f"Option {option}. {video_options[option]}")
-    
+
+    print()
     return video_options
 
 def customer_options():
@@ -68,6 +69,7 @@ def customer_options():
     for option in customer_options:
         print(f"Option {option}. {customer_options[option]}")
     
+    print()
     return customer_options
 
 def option_choice(options):
@@ -80,24 +82,34 @@ def option_choice(options):
     while choice not in valid_choices:
         print("Invalid option.")
         print("To see the options again, enter 9.")
-        choice = input("Enter a valid option number: ")
+        choice = input("Enter a valid option number: \n")
 
         if int(choice) == 9:
             menu_options()
 
-    
     return choice
 
 def video_tasks(choice):
     video_choice = int(choice)
+
     if video_choice == 1:
         # get all videos
         print_stars()
         print("Video Inventory\n")
         response = video_store.videos_index()
         print(response)
-    # elif video_choice == 2:
-    #     # search video by id or title
+
+    elif video_choice == 2:
+        # search video by id or title
+        print_stars()
+        print("Video Search by ID or Title\n")
+        id = input("Enter video ID: ")
+        title = input("Enter video title: ")
+
+        response = video_store.get_one_video(id=id, title=title)
+
+        print(response)
+
     elif video_choice == 3:
         # add a new video
         print_stars()
@@ -110,23 +122,88 @@ def video_tasks(choice):
         
         print(response)
 
-    # elif video_choice == 4:
-    #     # update video details
-    # elif video_choice == 5:
-    #     # delete a video
+    elif video_choice == 4:
+        # update video details
+        print_stars()
+        print("Update Video\n")
 
-# def customer_tasks(choice):
-#     customer_choice = int(choice)
-#     if customer_choice == 1:
-#         # get customer details by id, name, or phone #
-#     elif customer_choice == 2:
-#         # add new customer
-#     elif customer_choice == 3:
-#         # update customer information
-#     elif customer_choice == 4:
-#         # delete customer
-#     elif customer_choice == 5:
-#         # view all customers
+        id = input("Enter video ID: \n")
+
+        print("Enter changes below: \n")
+
+        title = input("Title: ")
+        release_date = input("Release Date: ")
+        total_inventory = input("Total Inventory: ")
+
+        response = video_store.update_video(id=id, title=title, release_date=release_date, total_inventory=total_inventory)
+        print(response)
+
+    elif video_choice == 5:
+        # delete a video
+        print("\n***WARNING: DELETING A VIDEO FROM THE INVENTORY CANNOT BE UNDONE***\n")
+        id = input("Enter video ID: \n")
+        confirm_id = input("Confirm video ID: \n")
+
+        response = video_store.delete_video(confirm_id)
+        print(response)
+
+def customer_tasks(choice):
+    customer_choice = int(choice)
+
+    if customer_choice == 1:
+        # get customer details by id, phone, or name
+        print_stars()
+        print("Customer Search by ID/Name/Phone\n")
+        id = input("Enter customer ID: ")
+        name = input("Enter customer name: ")
+        phone = input("Enter customer phone #: ")
+
+        response = video_store.get_one_customer(id=id, name=name, phone=phone)
+
+        print(response)
+    elif customer_choice == 2:
+        # add new customer
+        print_stars()
+        print("Enter new customer details: \n")
+        name = input("Enter customer name: ")
+        phone = input("Enter customer phone #: ")
+        postal_code = input("Enter customer zip code: ")
+
+        response = video_store.add_customer(name=name, phone=phone, postal_code=postal_code)
+        
+        print(response)
+    elif customer_choice == 3:
+        # update customer information
+        print_stars()
+        print("Update Customer Profile\n")
+
+        id = input("Enter customer ID: \n")
+
+        print("Update changes below: \n")
+
+        name = input("Enter customer name: ")
+        phone = input("Enter customer phone #: ")
+        postal_code = input("Enter customer zip code: ")
+
+        response = video_store.update_customer(id=id, name=name, phone=phone, postal_code=postal_code)
+        
+        print(response)
+
+    elif customer_choice == 4:
+        # delete customer
+        print("\n***WARNING: DELETING A CUSTOMER PROFILE CANNOT BE UNDONE***\n")
+        id = input("Enter customer ID: \n")
+        confirm_id = input("Confirm customer ID: \n")
+
+        response = video_store.delete_customer(confirm_id)
+        print(response)
+
+    elif customer_choice == 5:
+        # get all videos
+        print_stars()
+        print("Customer List\n")
+        response = video_store.customers_index()
+        print(response)
 
 def check_out():
 
@@ -147,6 +224,10 @@ def check_in():
     customer_id = input("Enter customer ID: ")
     video_id = input("Enter video ID: ")
 
+    response = video_store.check_in(customer_id=customer_id, video_id=video_id)
+
+    print(response)
+
 def run_cli(play=True):
 
     while play == True:
@@ -159,12 +240,14 @@ def run_cli(play=True):
             check_in()
 
         elif int(choice) == 3:
-            video_choice = option_choice(video_options)
-            video_tasks(video_choice)
+            choice = option_choice(video_options)
+            video_tasks(choice)
+            
 
         elif int(choice) == 4:
-            customer_choice = option_choice(customer_options)
-            customer_tasks(video_choice)
+            choice = option_choice(customer_options)
+            customer_tasks(choice)
+            
 
 def main():
     home_logo()
--- video_store.py
+++ video_store.py
@@ -11,11 +11,17 @@ class VideoStore:
             "video_id": video_id,
         }
 
-        response = requests.post(self.url+"/check-out", json=request_params)
+        response = requests.post(self.url+"/rentals/check-out", json=request_params)
         return response.json()
 
     def check_in(self, customer_id, video_id):
-        pass
+        request_params = {
+            "customer_id": customer_id,
+            "video_id": video_id,
+        }
+
+        response = requests.post(self.url+"/rentals/check-in", json=request_params)
+        return response.json()
 
     def add_video(self, title, release_date, total_inventory):
         request_params = {
@@ -32,26 +38,108 @@ class VideoStore:
         response = requests.get(self.url+"/videos")
         return response.json()
 
-    def get_one_video(self, id):
-        pass
+    def get_one_video(self, id=None, title=None):
+        try:
+            if id:
+                response = requests.get(self.url+f"/videos/{id}")
+                return response.json()
+            elif title:
+                inventory = requests.get(self.url+f"/videos")
+                for video in inventory.json():
+                
+                    if video.get("title") == title:
+                        return video
+        except ValueError:  
+            return f"=== Error: Video Not Found ==="
+                    
 
-    def update_video(self, id):
-        pass
+
+    def update_video(self, id=None, title=None, release_date=None, total_inventory=None):
+        try:
+            video = requests.get(self.url+f"/videos/{id}")
+            video_response = video.json()
+        except ValueError:
+            return f"=== Error: Video Not Found ==="
+
+        request_params = {
+                "title": title,
+                "release_date": release_date,
+                "total_inventory": total_inventory, 
+            }
+        if not title:
+            request_params["title"] = video_response.get("title")
+        if not release_date:
+            request_params["release_date"] = video_response.get("release_date")
+        if not total_inventory:
+            request_params["total_inventory"] = video_response.get("total_inventory")
+
+        updated_video = requests.put(self.url+f"/videos/{id}", json=request_params)
+        
+        return updated_video.json()
 
     def delete_video(self, id):
-        pass
+        try:
+            response = requests.delete(self.url+f"/videos/{id}")
+            return response.json()
+        except ValueError:
+            return f"=== Error: Video Not Found ==="
 
     def add_customer(self, name, postal_code, phone):
-        pass
+        request_params = {
+            "name": name,
+            "postal_code": postal_code,
+            "phone": phone,
+        }
 
-    def update_customer(self, id):
-        pass
+        response = requests.post(self.url+"/customers", json=request_params)
+        return response.json()
+
+
+    def update_customer(self, id=None, name=None, postal_code=None, phone=None):
+        customer = requests.get(self.url+f"/customers/{id}")
+        customer_response = customer.json()
+
+        request_params = {
+                "name": name,
+                "postal_code": postal_code,
+                "phone": phone, 
+            }
+        if not name:
+            request_params["name"] = customer_response.get("name")
+        if not postal_code:
+            request_params["postal_code"] = customer_response.get("postal_code")
+        if not phone:
+            request_params["phone"] = customer_response.get("phone")
+
+        updated_customer = requests.put(self.url+f"/customers/{id}", json=request_params)
+        
+        return updated_customer.json()
 
     def delete_customer(self, id):
-        pass
+        try:
+            response = requests.delete(self.url+f"/customers/{id}")
+            return response.json()
+        except ValueError:
+            return f"=== Error: Customer Not Found ==="
 
     def customers_index(self):
-        pass
+        response = requests.get(self.url+"/customers")
+        return response.json()
 
-    def get_one_customer(self, id):
-        pass
+    def get_one_customer(self, id=None, name=None, phone=None):
+        customers_index = requests.get(self.url+f"/customers")
+        try:
+            if id:
+                response = requests.get(self.url+f"/customers/{id}")
+                return response.json()
+            elif name:
+                for customer in customers_index.json():
+                    if customer.get("name") == name:
+                        return customer
+            elif phone:
+                for customer in customers_index.json():
+                    if customer.get("phone") == phone:
+                        return customer
+        except ValueError: 
+            return f"=== Error: Customer Not Found ==="
+
