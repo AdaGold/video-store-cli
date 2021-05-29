de462e00efa54f0a1caa0822faa197af10e1058f
SterlingSunshine <katrina.kimzey@gmail.com>
2021-05-28 20:28:28 -0700

    All required in and tested a bit

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from flask import json
 import requests
+from requests.api import request
 
 URL = "http://127.0.0.1:5000"
 BACKUP_URL = "https://retro-video-store-api.herokuapp.com"
+
 
 '''
     =============================================
@@ -41,48 +44,141 @@ def list_options_cust():
     options = {
 
     }
+    pass
 
 '''
     =============================================
-    ALL OPTION FUNCTIONS
+    EMPLOYEE OPTION FUNCTIONS
     =============================================
 '''
 
 def add_video():
-    pass
+    print("Enter video info below:")
+    request_body = {}
+    request_body["title"] = input("Title: ")
+    request_body["release_date"] = input("Release date: ")
+    request_body["total_inventory"] = input("Total inventory: ")
+
+    response = requests.post(URL +"/videos", json=request_body)
+    print(json.dumps(response.json(), indent=1))
+    return
 
 def edit_video():
-    pass
+    print("Enter updated video info below:")
+    request_body = {}
+    video_id = input("Video ID: ")
+    request_body["title"] = input("Title: ")
+    request_body["release_date"] = input("Release date: ")
+    request_body["total_inventory"] = input("Total inventory: ")
+
+    response = requests.put(URL +"/videos/" +video_id, json=request_body)
+    print(json.dumps(response.json(), indent=1))
+    return
 
 def remove_video():
-    pass
+    print("DELETE VIDEO - THIS ACTION CANNOT BE UNDONE")
+    if input("Are you sure? Y/N ") != "Y":
+        print("ACTION CANCELLED")
+        return
+    
+    video_id = input("Video ID: ")
+    response = requests.delete(URL +"/videos/" +video_id)
+    print(json.dumps(response.json(), indent=1))
+    return
 
 def view_video_stock():
-    pass
+    print("All Videos in Store Stock:")
+    response = requests.get(URL +"/videos")
+    print(json.dumps(response.json(), indent=2))
+    return
 
 def view_single_video():
-    pass
+    print("Video Info Request:")
+    video_id = input("Video ID: ")
+    response = requests.get(URL +"/videos/" +video_id)
+    print(json.dumps(response.json(), indent=1))
+    return
 
 def add_customer():
-    pass
+    print("Enter customer info below:")
+    request_body = {}
+    request_body["name"] = input("Name: ")
+    request_body["phone"] = input("Phone number: ")
+    request_body["postal_code"] = input("Postal code: ")
+
+    response = requests.post(URL +"/customers", json=request_body)
+    print(json.dumps(response.json(), indent=1))
+    return
 
 def edit_customer():
-    pass
+    print("Enter updated customer info below:")
+    request_body = {}
+    customer_id = input("Customer ID: ")
+    request_body["name"] = input("Name: ")
+    request_body["phone"] = input("Phone number: ")
+    request_body["postal_code"] = input("Postal code: ")
+    
+    response = requests.put(URL +"/customers/" +customer_id, json=request_body)
+    print(json.dumps(response.json(), indent=1))
+    return
 
 def delete_customer():
-    pass
+    print("DELETE CUSTOMER - THIS ACTION CANNOT BE UNDONE")
+    if input("Are you sure? Y/N ") != "Y":
+        print("ACTION CANCELLED")
+        return
+    
+    customer_id = input("Customer ID: ")
+    response = requests.delete(URL +"/customers/" +customer_id)
+    print(json.dumps(response.json(), indent=1))
+    return
 
 def view_customer():
-    pass
+    print("Customer Info Request:")
+    customer_id = input("Customer ID: ")
+    response = requests.get(URL +"/customers/" +customer_id)
+    print(json.dumps(response.json(), indent=1))
+    return
 
 def view_all_customers():
-    pass
+    print("All Active Customer Accounts:")
+    response = requests.get(URL +"/customers")
+    print(json.dumps(response.json(), indent=2))
+    return
 
 def checking_out():
-    pass
+    print("Check Out a Video:")
+    request_body = {}
+    request_body["customer_id"] = int(input("Customer ID: "))
+    request_body["video_id"] = int(input("Video ID: "))
+
+    response = requests.post(URL +"/rentals/check-out", json=request_body)
+    print(json.dumps(response.json(), indent=1))
+    return
 
 def checking_in():
-    pass
+    print("Check In a Video:")
+    request_body = {}
+    request_body["customer_id"] = int(input("Customer ID: "))
+    request_body["video_id"] = int(input("Video ID: "))
+
+    response = requests.post(URL +"/rentals/check-in", json=request_body)
+    print(json.dumps(response.json(), indent=1))
+    return
+
+'''
+    =============================================
+    CUSTOMER OPTION FUNCTIONS
+    =============================================
+'''
+
+def find_videos_by():
+    print("I'm sorry, that feature is not yet available in your area")
+    return
+
+def check_current_rentals():
+    print("I'm sorry, that feature is not yet available in your area")
+    return
 
 '''
     =============================================
@@ -97,7 +193,7 @@ def main(in_use=True, is_employee=False):
     if len(ee_id) == 4 and ee_id.isdigit():
         print(f"Welcome to work, Employee {ee_id}")
         is_employee = True
-        options = list_options_ee()
+        list_options_ee()
 
     while is_employee and in_use:
         func_call_dict = {
@@ -119,15 +215,28 @@ def main(in_use=True, is_employee=False):
         while choice not in func_call_dict:
             choice = input("What would you like to do? Q to quit.\n")
 
-        if choice == "Q" or choice == 'q':
-            print("Thank you for visiting the Retro Video Store!")
-            bar_break()
-            return
-        else:
-            func_call_dict[choice]()
+            if choice == "Q" or choice == 'q':
+                print(f"Goodbye Retro Video Store Employee {ee_id}!")
+                bar_break()
+                return
+        
+        func_call_dict[choice]()
+        bar_break()
     
     while in_use:
-        pass
+        func_call_dict = {
+            "1" : find_videos_by,
+            "2" : check_current_rentals
+        }
+
+        choice = None
+        while choice not in func_call_dict:
+            choice = input("What would you like to do? Q to quit.\n")
+
+            if choice == "Q" or choice == 'q':
+                print(f"Goodbye Retro Video Store Employee {ee_id}!")
+                bar_break()
+                return
 
 
 if __name__ == "__main__":
